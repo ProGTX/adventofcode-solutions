0340a49beeadd2d043ac953776036a7501be527a
Peter Žužek <peterzuzek@gmail.com>
2022-12-01 20:52:35 +0000

    2021 day 3 part 1 simpler

--- 2021/day03.cpp
+++ 2021/day03.cpp
@@ -17,27 +17,6 @@ template <size_t bit_width>
 using bits_array = std::array<int, bit_width>;
 
 template <size_t bit_width>
-using bits_lines_t = std::vector<bits_array<bit_width>>;
-
-template <size_t bit_width>
-using transposed_lines_t = std::array<std::vector<int>, bit_width>;
-
-template <size_t bit_width>
-transposed_lines_t<bit_width> transpose(
-    const bits_lines_t<bit_width>& container) {
-  transposed_lines_t<bit_width> transposed_lines;
-  for (int i = 0; i < bit_width; ++i) {
-    transposed_lines[i].reserve(container.size());
-  }
-  for (const auto& bits : container) {
-    for (int i = 0; i < bit_width; ++i) {
-      transposed_lines[i].push_back(bits[i]);
-    }
-  }
-  return transposed_lines;
-}
-
-template <size_t bit_width>
 int to_decimal(const bits_array<bit_width>& bits) {
   int decimal = 0;
   for (int multiplier = 1; auto bit : bits | std::ranges::views::reverse) {
@@ -49,29 +28,25 @@ int to_decimal(const bits_array<bit_width>& bits) {
 
 template <size_t bit_width>
 void solve_part1(const std::string& filename) {
-  bits_lines_t<bit_width> bits_lines;
+  std::array<std::array<int, 2>, bit_width> occurrences;
+  for (int i = 0; i < bit_width; ++i) {
+    occurrences[i] = {0, 0};
+  }
 
   readfile_op(filename, [&](std::string_view line) {
-    bits_array<bit_width> bits;
-    std::ranges::transform(line, std::begin(bits), [](unsigned char bit_char) {
-      return static_cast<int>(bit_char - '0');
-    });
-    bits_lines.push_back(bits);
+    for (int bitPos = 0; auto bit_char : line) {
+      auto bit = static_cast<int>(bit_char - '0');
+      ++occurrences[bitPos][bit];
+      ++bitPos;
+    }
   });
-
-  auto transposed_lines = transpose(bits_lines);
 
   bits_array<bit_width> gamma;
   bits_array<bit_width> epsilon;
-  for (int i = 0; const auto& downward_bits : transposed_lines) {
-    std::array occurrence{0, 0};
-    for (const auto bit : downward_bits) {
-      ++occurrence[bit];
-    }
-    const bool more_ones = (occurrence[1] >= occurrence[0]);
+  for (int i = 0; i < bit_width; ++i) {
+    const bool more_ones = (occurrences[i][1] >= occurrences[i][0]);
     gamma[i] = more_ones ? 1 : 0;
     epsilon[i] = more_ones ? 0 : 1;
-    ++i;
   }
 
   std::cout << filename << " -> " << (to_decimal(gamma) * to_decimal(epsilon))
