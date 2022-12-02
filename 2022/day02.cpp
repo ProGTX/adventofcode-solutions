d862816e5534913562ef84a96831d13d3da57a41
Peter Žužek <peterzuzek@gmail.com>
2022-12-02 10:22:03 +0000

    2022 day 2 part 2

--- 2022/day02.cpp
+++ 2022/day02.cpp
@@ -16,9 +16,17 @@
 #include "../common.h"
 
 enum hand {
-  rock = 1,
-  paper = 2,
-  scissors = 3,
+  // Starts at 0 instead of 1 so we can do modulo 3
+  // Need to add 1 to score accordingly
+  rock = 0,
+  paper = 1,
+  scissors = 2,
+};
+
+enum outcome {
+  lose = 0,
+  draw = 3,
+  win = 6,
 };
 
 void solve_part1(const std::string& filename) {
@@ -34,19 +42,50 @@ void solve_part1(const std::string& filename) {
         split<std::array<std::string, 2>>(std::string{line}, ' ');
     auto opponent = hand_map.at(opponentStr.at(0));
     auto response = hand_map.at(responseStr.at(0));
-    score += response;
+    // Add 1 because values are lower
+    score += response + 1;
     if (opponent == response) {
-      // Draw
-      score += 3;
+      score += draw;
       return;
     }
     if (((response == rock) && (opponent == scissors)) ||
         ((response - opponent) == 1)) {
-      // Win
-      score += 6;
+      score += win;
     } else {
-      // Lose
+      score += lose;
     }
+  });
+
+  std::cout << filename << " -> " << score << std::endl;
+}
+
+void solve_part2(const std::string& filename) {
+  int score = 0;
+
+  std::map<char, int> hand_map = {
+      {'A', rock}, {'B', paper}, {'C', scissors},
+      {'X', lose}, {'Y', draw},  {'Z', win},
+  };
+
+  readfile_op(filename, [&](std::string_view line) {
+    auto [opponentStr, outcomeStr] =
+        split<std::array<std::string, 2>>(std::string{line}, ' ');
+    auto opponent = hand_map.at(opponentStr.at(0));
+    auto outcome = hand_map.at(outcomeStr.at(0));
+    score += outcome;
+    if (outcome == draw) {
+      // Add 1 because values are lower
+      score += opponent + 1;
+      return;
+    }
+    int response = 0;
+    if (outcome == win) {
+      response = ((opponent + 1) % 3);
+    } else {
+      response = ((3 + opponent - 1) % 3);
+    }
+    // Add 1 because values are lower
+    score += response + 1;
   });
 
   std::cout << filename << " -> " << score << std::endl;
@@ -56,7 +95,7 @@ int main() {
   std::cout << "Part 1" << std::endl;
   solve_part1("day02.example");
   solve_part1("day02.input");
-  // std::cout << "Part 2" << std::endl;
-  // solve_case("day02.example");
-  // solve_case("day02.input");
+  std::cout << "Part 2" << std::endl;
+  solve_part2("day02.example");
+  solve_part2("day02.input");
 }
