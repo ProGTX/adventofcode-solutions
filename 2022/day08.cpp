57e3c3a33aa8ec9f657db48d049ac4338b8cd7be
Peter Žužek <peterzuzek@gmail.com>
2022-12-14 13:14:10 +0000

    2022 day 8 part 1

--- 2022/day08.cpp
+++ 2022/day08.cpp
@@ -14,14 +14,94 @@
 
 #include "../common.h"
 
+class grid {
+ public:
+  using row_t = std::vector<int>;
+  using data_t = std::vector<int>;
+  using iterator = typename data_t::iterator;
+  using const_iterator = typename data_t::const_iterator;
+
+  iterator add_row(const row_t& row) {
+    auto old_size = data.size();
+    row_length = row.size();
+    data.reserve(old_size + row_length);
+    for (auto value : row) {
+      data.push_back(value);
+      visible.push_back(0);
+    }
+    ++num_rows;
+    return std::begin(data) + old_size;
+  }
+
+  void check_visibility() {
+    if (num_rows != row_length) {
+      throw std::runtime_error("Only square grids are supported");
+    }
+
+    // Create walls that we move from one side to another
+    // Each tree can only increase the height of the wall
+    // 0 is a valid tree height, so initialize wall to -1
+    // ltr - left-to-right, rtl - right-to-left
+    // ttb - top-to-bottom, btt - bottom-to-top
+    row_t ltr_wall(num_rows, -1);
+    row_t rtl_wall(num_rows, -1);
+    row_t ttb_wall(row_length, -1);
+    row_t btt_wall(row_length, -1);
+
+    const auto update_wall = [this](int data_index, row_t& wall,
+                                    int wall_index) {
+      int tree_height = data[data_index];
+      if (tree_height > wall[wall_index]) {
+        ++visible[data_index];
+        wall[wall_index] = tree_height;
+      }
+    };
+
+    for (int ltr = 0, ttb = 0; ltr < row_length; ++ltr, ++ttb) {
+      for (int wall_index = 0; wall_index < num_rows; ++wall_index) {
+        int ltr_index = wall_index * row_length + ltr;
+        update_wall(ltr_index, ltr_wall, wall_index);
+
+        int rtl_index = (wall_index + 1) * row_length - ltr - 1;
+        update_wall(rtl_index, rtl_wall, wall_index);
+
+        int ttb_index = ttb * row_length + wall_index;
+        update_wall(ttb_index, ttb_wall, wall_index);
+
+        int btt_index = (num_rows - ttb - 1) * row_length + wall_index;
+        update_wall(btt_index, btt_wall, wall_index);
+      }
+    }
+  }
+
+  int num_visible() const {
+    return std::ranges::count_if(visible,
+                                 [](int value) { return (value > 0); });
+  }
+
+ private:
+  data_t data;
+  std::vector<int> visible;
+  int row_length = 0;
+  int num_rows = 0;
+};
+
 template <bool>
 void solve_case(const std::string& filename) {
-  int score = 0;
+  grid forrest;
+  grid::row_t row;
 
   readfile_op(filename, [&](std::string_view line) {
+    row.clear();
+    for (auto number_str : line) {
+      row.push_back(static_cast<int>(number_str - '0'));
+    }
+    forrest.add_row(row);
   });
 
-  std::cout << filename << " -> " << score << std::endl;
+  forrest.check_visibility();
+
+  std::cout << filename << " -> " << forrest.num_visible() << std::endl;
 }
 
 int main() {
@@ -29,6 +109,6 @@ int main() {
   solve_case<false>("day08.example");
   solve_case<false>("day08.input");
   // std::cout << "Part 2" << std::endl;
-  solve_case<true>("day08.example");
-  solve_case<true>("day08.input");
+  // solve_case<true>("day08.example");
+  // solve_case<true>("day08.input");
 }
