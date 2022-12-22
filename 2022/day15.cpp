143491be3ca415a8c382932f35be1e72729889e0
Peter Žužek <peterzuzek@gmail.com>
2022-12-22 23:36:07 +0100

    2022 day 15 part 2

--- 2022/day15.cpp
+++ 2022/day15.cpp
@@ -4,10 +4,12 @@
 #include <array>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <memory>
 #include <numeric>
 #include <ostream>
 #include <ranges>
+#include <set>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -24,9 +26,107 @@ struct sensor_t {
   }
 };
 
+struct bounds_t {
+  int min{0};
+  int max{0};
+
+  friend bool operator==(const bounds_t&, const bounds_t&) = default;
+
+  friend bool operator<(const bounds_t& lhs, const bounds_t& rhs) {
+    if (lhs.min < rhs.min) {
+      return true;
+    }
+    return (lhs.min == rhs.min) && (lhs.max < rhs.max);
+  }
+};
+
 using beacon_t = point;
 
-template <int inspect_row, bool>
+template <bool find_distress>
+std::vector<point> find_positions(const std::vector<sensor_t>& sensors,
+                                  const std::vector<beacon_t>& beacons,
+                                  const bounds_t& bounds, int inspect_row) {
+  std::vector<sensor_t> sensors_on_row;
+  std::ranges::copy_if(
+      sensors, std::back_inserter(sensors_on_row),
+      [&](const sensor_t& sensor) { return (sensor.pos.y == inspect_row); });
+  std::vector<beacon_t> beacons_on_row;
+  std::ranges::copy_if(
+      beacons, std::back_inserter(beacons_on_row),
+      [&](const beacon_t& beacon) { return (beacon.y == inspect_row); });
+
+  std::vector<point> positions;
+
+  for (int column = bounds.min; column <= bounds.max; ++column) {
+    point current{column, inspect_row};
+    bool within_range =
+        std::ranges::any_of(sensors, [&](const sensor_t& sensor) {
+          return (point::distance_manhattan(sensor.pos, current) <=
+                  sensor.range);
+        });
+    if (within_range == find_distress) {
+      continue;
+    }
+    bool is_empty =
+        !std::ranges::any_of(sensors_on_row,
+                             [&](const sensor_t& sensor) {
+                               return sensor.pos.x == current.x;
+                             }) &&
+        !std::ranges::any_of(beacons_on_row, [&](const beacon_t& beacon) {
+          return beacon.x == current.x;
+        });
+    if (is_empty) {
+      positions.push_back(current);
+    }
+  }
+
+  return positions;
+}
+
+beacon_t find_distress_beacon(const std::vector<sensor_t>& sensors,
+                              const bounds_t& bounds) {
+  std::map<int, std::set<bounds_t>> row_exclusions;
+
+  const auto add_exclusion = [&](int row, const sensor_t& sensor, int width) {
+    if ((row < bounds.min) || (row > bounds.max)) {
+      return;
+    }
+    bounds_t exclusion{std::max(sensor.pos.x - width, bounds.min),
+                       std::min(sensor.pos.x + width, bounds.max)};
+    if (exclusion.min > exclusion.max) {
+      return;
+    }
+    row_exclusions[row].insert(exclusion);
+  };
+
+  // Collect exclusion zones for each row
+  for (const sensor_t& sensor : sensors) {
+    for (int row = sensor.pos.y - sensor.range, width = 0; row < sensor.pos.y;
+         ++row, ++width) {
+      add_exclusion(row, sensor, width);
+    }
+    for (int row = sensor.pos.y, width = sensor.range;
+         row <= sensor.pos.y + sensor.range; ++row, --width) {
+      add_exclusion(row, sensor, width);
+    }
+  }
+
+  // Try to find a gap in the exclusion zones
+  for (const auto& [row, exclusions] : row_exclusions) {
+    bounds_t previous{bounds.min, bounds.min};
+    for (const auto& row_exclusion : exclusions) {
+      if (row_exclusion.min - previous.max > 0) {
+        return point{previous.max + 1, row};
+      }
+      previous = bounds_t{row_exclusion.min,
+                          std::max(row_exclusion.max, previous.max)};
+    }
+  }
+
+  return {};
+}
+
+template <int inspect_row, bool find_distress>
 void solve_case(const std::string& filename) {
   std::vector<sensor_t> sensors;
   std::vector<beacon_t> beacons;
@@ -63,54 +163,36 @@ void solve_case(const std::string& filename) {
     }
   });
 
-  decltype(sensors) sensors_on_row;
-  std::ranges::copy_if(
-      sensors, std::back_inserter(sensors_on_row),
-      [](const sensor_t& sensor) { return (sensor.pos.y == inspect_row); });
-  const auto largest_distance =
-      1000 * // TODO: why do I need to go so high?
-      std::ranges::max(sensors, [](const sensor_t& lhs, const sensor_t& rhs) {
-        return lhs.range > rhs.range;
-      }).range;
+  // Result for part 2 is huge, store it into a long
+  long score = 0;
 
-  decltype(beacons) beacons_on_row;
-  std::ranges::copy_if(
-      beacons, std::back_inserter(beacons_on_row),
-      [](const beacon_t& beacon) { return (beacon.y == inspect_row); });
-
-  int num_empty = 0;
-  for (int column = min_max.min_value.x - largest_distance;
-       column <= min_max.max_value.x + largest_distance; ++column) {
-    point current{column, inspect_row};
-    bool within_range =
-        std::ranges::any_of(sensors, [&](const sensor_t& sensor) {
-          return (point::distance_manhattan(sensor.pos, current) <=
-                  sensor.range);
-        });
-    if (!within_range) {
-      continue;
-    }
-    bool is_empty =
-        !std::ranges::any_of(sensors_on_row,
-                             [&](const sensor_t& sensor) {
-                               return sensor.pos.x == current.x;
-                             }) &&
-        !std::ranges::any_of(beacons_on_row, [&](const beacon_t& beacon) {
-          return beacon.x == current.x;
-        });
-    if (is_empty) {
-      ++num_empty;
-    }
+  if constexpr (!find_distress) {
+    auto max_range =
+        std::ranges::max(sensors, [](const sensor_t& lhs, const sensor_t& rhs) {
+          return lhs.range < rhs.range;
+        }).range;
+    const auto largest_distance = 2 * max_range + 1;
+    auto positions =
+        find_positions<find_distress>(sensors, beacons,
+                                      {min_max.min_value.x - largest_distance,
+                                       min_max.max_value.x + largest_distance},
+                                      inspect_row);
+    score = positions.size();
+  } else {
+    constexpr long multiplier = 4000000;
+    const int max_pos = (inspect_row == 10) ? 20 : multiplier;
+    auto beacon = find_distress_beacon(sensors, {0, max_pos});
+    score = beacon.x * multiplier + beacon.y;
   }
 
-  std::cout << filename << " -> " << num_empty << std::endl;
+  std::cout << filename << " -> " << score << std::endl;
 }
 
 int main() {
   std::cout << "Part 1" << std::endl;
   solve_case<10, false>("day15.example");
   solve_case<2000000, false>("day15.input");
-  // std::cout << "Part 2" << std::endl;
-  // solve_case<10, true>("day15.example");
-  // solve_case<2000000, true>("day15.input");
+  std::cout << "Part 2" << std::endl;
+  solve_case<10, true>("day15.example");
+  solve_case<2000000, true>("day15.input");
 }
