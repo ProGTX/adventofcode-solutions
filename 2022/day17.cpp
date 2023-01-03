e81ffc9a165cbf8dfb3e867fd3f9d45ac755a623
Peter Žužek <peterzuzek@gmail.com>
2023-01-03 09:41:32 +0100

    2022 day 17 part 2 setup

--- 2022/day17.cpp
+++ 2022/day17.cpp
@@ -2,6 +2,7 @@
 
 #include "../common/common.h"
 #include "../common/grid.h"
+#include "../common/print.h"
 
 #include <algorithm>
 #include <array>
@@ -15,35 +16,39 @@
 #include <string_view>
 #include <vector>
 
-using rock_t = std::array<point, 5>;
+using rock_index_t = std::int64_t;
+using grid_point = point_t<rock_index_t>;
+using rock_t = std::array<grid_point, 5>;
 
-constexpr void move_rock(rock_t& rock, point move) {
-  for (point& p : rock) {
+constexpr void move_rock(rock_t& rock, grid_point move) {
+  for (grid_point& p : rock) {
     p += move;
   }
 }
 
-template <int num_rocks>
-int get_final_height(const std::vector<int>& pattern) {
+template <rock_index_t num_rocks>
+rock_index_t get_final_height(const std::vector<int>& pattern) {
+  static constexpr bool use_sparse_grid = num_rocks > 10000;
+
   constexpr int left_most_index = 0;
   constexpr int right_most_index = 4;
   constexpr int top_most_index = 1;
   constexpr int bottom_most_index = 3;
 
-  static constexpr auto initial_rock_displacement = point{2, 3};
+  static constexpr auto initial_rock_displacement = grid_point{2, 3};
 
   constexpr std::array<rock_t, 5> rock_structures = std::invoke([] {
     std::array<rock_t, 5> rocks;
-    rocks[0] = {point{0, 0}, point{1, 0}, point{2, 0}, point{3, 0},
-                point{3, 0}};
-    rocks[1] = {point{0, 1}, point{1, 2}, point{1, 1}, point{1, 0},
-                point{2, 1}};
-    rocks[2] = {point{0, 0}, point{2, 2}, point{2, 0}, point{1, 0},
-                point{2, 1}};
-    rocks[3] = {point{0, 1}, point{0, 3}, point{0, 0}, point{0, 0},
-                point{0, 2}};
-    rocks[4] = {point{0, 0}, point{0, 1}, point{0, 0}, point{1, 0},
-                point{1, 1}};
+    rocks[0] = {grid_point{0, 0}, grid_point{1, 0}, grid_point{2, 0},
+                grid_point{3, 0}, grid_point{3, 0}};
+    rocks[1] = {grid_point{0, 1}, grid_point{1, 2}, grid_point{1, 1},
+                grid_point{1, 0}, grid_point{2, 1}};
+    rocks[2] = {grid_point{0, 0}, grid_point{2, 2}, grid_point{2, 0},
+                grid_point{1, 0}, grid_point{2, 1}};
+    rocks[3] = {grid_point{0, 1}, grid_point{0, 3}, grid_point{0, 0},
+                grid_point{0, 0}, grid_point{0, 2}};
+    rocks[4] = {grid_point{0, 0}, grid_point{0, 1}, grid_point{0, 0},
+                grid_point{1, 0}, grid_point{1, 1}};
 
     // Each rock appears so that its left edge is two units away
     // from the left wall and its bottom edge is three units above
@@ -63,7 +68,15 @@ int get_final_height(const std::vector<int>& pattern) {
 
   // Just use rocks as the walls, no need to do bounds checking
   using chamber_row_t = std::array<char, chamber_width + 2>;
-  grid<char, chamber_row_t, std::vector<char>> chamber;
+
+  auto chamber = std::invoke([]() {
+    if constexpr (use_sparse_grid) {
+      return sparse_grid<char, grid_point, chamber_row_t>{};
+    } else {
+      return grid<char, chamber_row_t, std::vector<char>>{};
+    }
+  });
+
   // And use rocks for the floor
   chamber_row_t floor;
   std::ranges::fill(floor, rock_chars[0]);
@@ -76,10 +89,11 @@ int get_final_height(const std::vector<int>& pattern) {
     return new_row;
   });
 
-  const auto try_move_rock = [&](rock_t& falling_rock, point move) -> bool {
+  const auto try_move_rock = [&](rock_t& falling_rock,
+                                 grid_point move) -> bool {
     move_rock(falling_rock, move);
     bool intersects_with_rock =
-        std::ranges::any_of(falling_rock, [&](const point& rp) {
+        std::ranges::any_of(falling_rock, [&](const grid_point& rp) {
           const auto value = chamber.at(rp.y, rp.x);
           return std::ranges::any_of(
               rock_chars, [&](char rock_char) { return (value == rock_char); });
@@ -105,13 +119,14 @@ int get_final_height(const std::vector<int>& pattern) {
       out << value;
     });
   };
+  (void)print_chamber;
 
-  int current_height = 1;
+  rock_index_t current_height = 1;
   int pattern_index = 0;
-  for (int r = 0; r < num_rocks; ++r) {
+  const auto rock_solver = [&](const int rock_index) {
     rock_char_index = (rock_char_index + 1) % rock_chars.size();
 
-    rock_t falling_rock = rock_structures[r % rock_structures.size()];
+    rock_t falling_rock = rock_structures[rock_index % rock_structures.size()];
     const auto current_rock_height =
         falling_rock[top_most_index].y - falling_rock[bottom_most_index].y + 1;
 
@@ -122,7 +137,7 @@ int get_final_height(const std::vector<int>& pattern) {
       auto leftmost = falling_rock[left_most_index].x + sideways;
       auto rightmost = falling_rock[right_most_index].x + sideways;
       if ((leftmost >= 0) && (rightmost < chamber_width)) {
-        move_rock(falling_rock, point{sideways, 0});
+        move_rock(falling_rock, grid_point{sideways, 0});
       }
       pattern_index = (pattern_index + 1) % pattern.size();
     }
@@ -136,16 +151,16 @@ int get_final_height(const std::vector<int>& pattern) {
       // This move includes the vertical movement from the initial falling stage
       // It also includes the left chamber wall
       move_rock(falling_rock,
-                point{1, current_height - initial_rock_displacement.y});
+                grid_point{1, current_height - initial_rock_displacement.y});
     }
 
     // Move rock until it settles
     while (true) {
       auto sideways = pattern[pattern_index];
-      try_move_rock(falling_rock, point{sideways, 0});
-      if (!try_move_rock(falling_rock, point{0, -1})) {
+      try_move_rock(falling_rock, grid_point{sideways, 0});
+      if (!try_move_rock(falling_rock, grid_point{0, -1})) {
         // Rock settled, engrave it into the chamber
-        for (const point& rp : falling_rock) {
+        for (const grid_point& rp : falling_rock) {
           auto actual_rock_char_index =
               (rock_char_index % (rock_chars.size() - 1)) + 1;
           chamber.modify(rock_chars[actual_rock_char_index], rp.y, rp.x);
@@ -157,13 +172,83 @@ int get_final_height(const std::vector<int>& pattern) {
       }
       pattern_index = (pattern_index + 1) % pattern.size();
     }
-  }
+  };
 
-  return current_height - 1;
+  // Perform iterations
+
+  const int cycle_size =
+      std::min(static_cast<size_t>(num_rocks),
+               std::lcm(rock_structures.size(), pattern.size()));
+  int rock_index = 0;
+
+  rock_index_t initial_height = 0;
+  for (int r = 0; r < cycle_size; ++r) {
+    rock_solver(rock_index);
+    ++rock_index;
+  }
+  initial_height = current_height - 1;
+  const chamber_row_t initial_top_row = chamber.get_row(current_height - 1);
+  print_range(initial_top_row) << std::endl;
+
+  const auto is_same_as_initial = [&](const chamber_row_t& current_top_row) {
+    for (int c = 1; c < initial_top_row.size() - 1; ++c) {
+      if (current_top_row[c] == initial_top_row[c]) {
+        continue;
+      }
+      if ((current_top_row[c] != empty_char) &&
+          (initial_top_row[c] != empty_char)) {
+        continue;
+      }
+      return false;
+    }
+    return true;
+  };
+
+  rock_index_t height_per_extended_cycle = 0;
+  int num_cycle_repetitions = 0;
+  while (rock_index < num_rocks) {
+    for (int r = 0; r < cycle_size; ++r) {
+      rock_solver(rock_index);
+      ++rock_index;
+    }
+    auto current_top_row = chamber.get_row(current_height - 1);
+    print_range(current_top_row) << std::endl;
+    if (is_same_as_initial(current_top_row)) {
+      break;
+    }
+    ++num_cycle_repetitions;
+  }
+  height_per_extended_cycle = current_height - 1 - initial_height;
+
+  rock_index_t leftover_height = 0;
+  int leftover_height_index =
+      (num_rocks % (cycle_size * num_cycle_repetitions));
+  for (int r = 0; r < leftover_height_index; ++r) {
+    rock_solver(rock_index);
+    ++rock_index;
+  }
+  leftover_height =
+      current_height - 1 - height_per_extended_cycle - initial_height;
+
+  const auto num_repetitions =
+      num_rocks / std::max(1, (cycle_size * num_cycle_repetitions));
+
+  std::cout << "  cycle_size " << cycle_size << std::endl;
+  std::cout << "  initial_height " << initial_height << std::endl;
+  std::cout << "  num_repetitions " << num_repetitions << std::endl;
+  std::cout << "  num_cycle_repetitions " << num_cycle_repetitions << std::endl;
+  std::cout << "  height_per_extended_cycle " << height_per_extended_cycle
+            << std::endl;
+  std::cout << "  leftover_height " << leftover_height << std::endl;
+
+  return initial_height +
+         height_per_extended_cycle *
+             std::max(rock_index_t{0}, num_repetitions - 1) +
+         leftover_height;
 }
 
-template <int num_rocks, bool>
-void solve_case(const std::string& filename) {
+template <rock_index_t num_rocks>
+rock_index_t solve_case(const std::string& filename) {
   std::vector<int> pattern;
 
   readfile_op(filename, [&](std::string_view line) {
@@ -179,15 +264,17 @@ void solve_case(const std::string& filename) {
     });
   });
 
-  int final_height = get_final_height<num_rocks>(pattern);
+  rock_index_t final_height = get_final_height<num_rocks>(pattern);
   std::cout << filename << " -> " << final_height << std::endl;
+  return final_height;
 }
 
 int main() {
   std::cout << "Part 1" << std::endl;
-  solve_case<2022, false>("day17.example");
-  solve_case<2022, false>("day17.input");
-  // std::cout << "Part 2" << std::endl;
-  // solve_case<2022, true>("day17.example");
-  // solve_case<2022, true>("day17.input");
+  EXPECT_RESULT(3068, solve_case<2022>("day17.example"));
+  EXPECT_RESULT(3085, solve_case<2022>("day17.input"));
+  std::cout << "Part 2" << std::endl;
+  solve_case<1000000000000>("day17.example");
+  // solve_case<1000000000000>("day17.input");
+  RETURN_CHECK_RESULT();
 }
