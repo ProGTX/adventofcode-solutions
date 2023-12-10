5907668109354c75827a8f8df3d344ac2f55c606
Peter Žužek <peterzuzek@gmail.com>
2023-12-10 10:09:36 +0100

    2023 day 4 part 2

--- 2023/day04.cpp
+++ 2023/day04.cpp
@@ -35,26 +35,33 @@ struct card_type {
 
 // Requires the card to be sorted
 template <int num_winning, int num_actual>
-constexpr int card_value(const card_type<num_winning, num_actual>& card) {
-  typename card_type<num_winning, num_actual>::winning_t intersection{0, 0, 0,
-                                                                      0, 0};
+constexpr int num_matching(const card_type<num_winning, num_actual>& card) {
+  using intersection_t = typename card_type<num_winning, num_actual>::winning_t;
+  intersection_t intersection{};
+  std::ranges::fill(intersection, 0);
   std::ranges::set_intersection(card.winning, card.actual,
                                 std::begin(intersection));
-  auto num_winning_numbers =
-      std::ranges::count_if(intersection, std::identity{});
-  if (num_winning_numbers == 0) {
+  return std::ranges::count_if(intersection, std::identity{});
+}
+
+template <int num_winning, int num_actual>
+constexpr int card_value(const card_type<num_winning, num_actual>& card) {
+  auto matching = num_matching(card);
+  if (matching == 0) {
     return 0;
   }
-  return pown(2, (num_winning_numbers - 1));
+  return pown(2, (matching - 1));
 }
 
 static_assert(8 == card_value(card_type<5, 8>{{41, 48, 83, 86, 17},
                                               {83, 86, 6, 31, 17, 9, 48, 53}}));
 
-template <int num_winning, int num_actual>
+template <int num_winning, int num_actual, bool count_scratchcards>
 int solve_case(const std::string& filename) {
+  std::vector<int> num_scratchcards(1, 0);
   int sum = 0;
-  auto solver = [&](std::string_view line) {
+
+  auto solver = [&](std::string_view line, int linenum) {
     auto [card_id, numbers] = split<std::array<std::string_view, 2>>(line, ':');
     auto [winning_str, actual_str] =
         split<std::array<std::string_view, 2>>(numbers, '|');
@@ -62,7 +69,25 @@ int solve_case(const std::string& filename) {
     card_t current_card{
         split<typename card_t::winning_t, true>(winning_str, ' '),
         split<typename card_t::actual_t, true>(actual_str, ' ')};
-    sum += card_value(current_card);
+
+    if constexpr (!count_scratchcards) {
+      sum += card_value(current_card);
+    } else {
+      // Add the original card
+      ++num_scratchcards[linenum - 1];
+      int current_num = num_scratchcards[linenum - 1];
+      sum += current_num;
+
+      int matching = num_matching(current_card);
+      // Create copies of next cards
+      int required_scratchcards_size = linenum + matching;
+      if (num_scratchcards.size() < required_scratchcards_size) {
+        num_scratchcards.resize(required_scratchcards_size);
+      }
+      for (int i = 0; i < matching; ++i) {
+        num_scratchcards[linenum + i] += current_num;
+      }
+    }
   };
 
   readfile_op(filename, solver);
@@ -73,10 +98,10 @@ int solve_case(const std::string& filename) {
 
 int main() {
   std::cout << "Part 1" << std::endl;
-  AOC_EXPECT_RESULT(13, (solve_case<5, 8>("day04.example")));
-  AOC_EXPECT_RESULT(32609, (solve_case<10, 25>("day04.input")));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(467835, solve_case<true>("day04.example"));
-  // AOC_EXPECT_RESULT(84883664, solve_case<true>("day04.input"));
+  AOC_EXPECT_RESULT(13, (solve_case<5, 8, false>("day04.example")));
+  AOC_EXPECT_RESULT(32609, (solve_case<10, 25, false>("day04.input")));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(30, (solve_case<5, 8, true>("day04.example")));
+  AOC_EXPECT_RESULT(14624680, (solve_case<10, 25, true>("day04.input")));
   AOC_RETURN_CHECK_RESULT();
 }
