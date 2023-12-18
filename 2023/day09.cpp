602588e25b743691d5d74b0c767ba9478e3e56b6
Peter Žužek <peterzuzek@gmail.com>
2023-12-18 10:52:59 +0000

    2023 day 9 part 2

--- 2023/day09.cpp
+++ 2023/day09.cpp
@@ -12,14 +12,21 @@
 using namespace std::string_view_literals;
 using int_t = int;
 
+template <int multiplier = 1>
 constexpr int predict_value(std::vector<int> reading) {
-  std::vector<int> last_values;
-  std::vector<int> next_reading;
+  static_assert(multiplier != 0);
+
+  std::vector<int> edge_values;
+  std::vector<int> next_reading = reading;
   int size = reading.size();
-  last_values.reserve(size);
-  next_reading.reserve(size);
-  last_values.push_back(reading.back());
+  edge_values.reserve(size);
+
   while (size > 0) {
+    if constexpr (multiplier > 0) {
+      edge_values.push_back(reading.back());
+    } else {
+      edge_values.push_back(reading.front());
+    }
     next_reading.clear();
     for (int i = 0; i < size - 1; ++i) {
       next_reading.push_back(reading[i + 1] - reading[i]);
@@ -28,13 +35,12 @@ constexpr int predict_value(std::vector<int> reading) {
                             [](int value) { return value == 0; })) {
       break;
     }
-    last_values.push_back(next_reading.back());
     std::swap(next_reading, reading);
     --size;
   }
   int prediction = 0;
-  for (int i = last_values.size() - 1; i >= 0; --i) {
-    prediction += last_values[i];
+  for (int i = edge_values.size() - 1; i >= 0; --i) {
+    prediction = edge_values[i] + prediction * multiplier;
   }
   return prediction;
 }
@@ -42,16 +48,20 @@ constexpr int predict_value(std::vector<int> reading) {
 static_assert(18 == predict_value({0, 3, 6, 9, 12, 15}));
 static_assert(28 == predict_value({1, 3, 6, 10, 15, 21}));
 static_assert(68 == predict_value({10, 13, 16, 21, 30, 45}));
+static_assert(-3 == predict_value<-1>({0, 3, 6, 9, 12, 15}));
+static_assert(0 == predict_value<-1>({1, 3, 6, 10, 15, 21}));
+static_assert(5 == predict_value<-1>({10, 13, 16, 21, 30, 45}));
 
+template <int multiplier>
 constexpr int sum_predictions(const std::vector<std::vector<int>>& values) {
   return ranges::fold_left(
       values | std::views::transform([](const std::vector<int>& reading) {
-        return predict_value(reading);
+        return predict_value<multiplier>(reading);
       }),
       int_t{0}, std::plus<>{});
 }
 
-template <bool>
+template <int multiplier>
 int solve_case(const std::string& filename) {
   std::vector<std::vector<int>> values;
   auto read_values = [&](std::string_view line) {
@@ -59,17 +69,17 @@ int solve_case(const std::string& filename) {
   };
   readfile_op(filename, read_values);
 
-  int sum = sum_predictions(values);
+  int sum = sum_predictions<multiplier>(values);
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
 }
 
 int main() {
   std::cout << "Part 1" << std::endl;
-  AOC_EXPECT_RESULT(114, (solve_case<false>("day09.example")));
-  AOC_EXPECT_RESULT(1980437560, (solve_case<false>("day09.input")));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(5905, (solve_case<true>("day09.example")));
-  // AOC_EXPECT_RESULT(249776650, (solve_case<true>("day09.input")));
-  // AOC_RETURN_CHECK_RESULT();
+  AOC_EXPECT_RESULT(114, (solve_case<1>("day09.example")));
+  AOC_EXPECT_RESULT(1980437560, (solve_case<1>("day09.input")));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(2, (solve_case<-1>("day09.example")));
+  AOC_EXPECT_RESULT(977, (solve_case<-1>("day09.input")));
+  AOC_RETURN_CHECK_RESULT();
 }
