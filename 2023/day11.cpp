bf8792d14ef05f6df4b43e63cffb14f10e8ef25d
Peter Žužek <peterzuzek@gmail.com>
2023-12-31 10:59:11 +0100

    2023 day 11 part 2

--- 2023/day11.cpp
+++ 2023/day11.cpp
@@ -12,83 +12,93 @@
 
 using namespace std::string_view_literals;
 
+using int_t = std::int64_t;
+using point_t = point_type<int_t>;
+
 inline constexpr char galaxy = '#';
 inline constexpr char empty_space = '.';
-using space_t = sparse_grid<char, empty_space>;
+using space_t = sparse_grid<char, empty_space, point_t>;
 
-space_t expand_columns(const std::vector<std::string>& space_rows,
-                       const std::vector<bool>& column_full) {
+template <int factor>
+space_t expand_space(const std::vector<std::string>& space_rows,
+                     const std::vector<bool>& row_populated,
+                     const std::vector<bool>& column_populated) {
   AOC_ASSERT(space_rows.size() > 1, "Cannot construct space from no rows");
-  const auto num_empty_columns = std::ranges::count(column_full, false);
+  const auto num_empty_columns = std::ranges::count(column_populated, false);
   const auto orig_row_size = space_rows[0].size();
   const auto num_columns = orig_row_size + num_empty_columns;
   auto space = space_t(space_rows.size(), num_columns);
-  int row = 0;
-  for (const auto& current_row : space_rows) {
-    int column = 0;
+  int_t row = 0;
+  for (int r = 0; r < space_rows.size(); ++r, ++row) {
+    if (!row_populated[r]) {
+      row += factor - 1;
+      continue;
+    }
+    const auto& current_row = space_rows[r];
+    int_t column = 0;
     for (int c = 0; c < orig_row_size; ++c, ++column) {
-      if (!column_full[c]) {
-        ++column;
+      if (!column_populated[c]) {
+        column += factor - 1;
         continue;
       } else if (current_row[c] != galaxy) {
         continue;
       }
       space.modify(galaxy, row, column);
     }
-    ++row;
   }
   return space;
 }
 
-int sum_distances(const space_t& space) {
-  int sum = 0;
+int_t sum_distances(const space_t& space) {
+  int_t sum = 0;
   for (auto it1 = std::begin(space); it1 != std::end(space); ++it1) {
     auto it2 = it1;
     for (++it2; it2 != std::end(space); ++it2) {
-      sum += point::distance_manhattan(it1->first, it2->first);
+      sum += point_t::distance_manhattan(it1->first, it2->first);
     }
   }
   return sum;
 }
 
-template <bool>
-int solve_case(const std::string& filename) {
+template <int factor>
+int_t solve_case(const std::string& filename) {
   std::vector<std::string> space_rows;
-  std::vector<bool> column_full;
   std::string empty_row;
+  std::vector<bool> column_populated;
+  std::vector<bool> row_populated;
 
   auto read_values = [&](std::string&& line) {
     const auto size = line.size();
     if (empty_row.empty()) {
       empty_row.reserve(size);
-      column_full.reserve(size);
+      column_populated.reserve(size);
       std::ranges::fill_n(std::back_inserter(empty_row), size, empty_space);
-      std::ranges::fill_n(std::back_inserter(column_full), size, false);
+      std::ranges::fill_n(std::back_inserter(column_populated), size, false);
     }
-    if (line == empty_row) {
-      // Extra insertion to expand the row
-      space_rows.push_back(empty_row);
-    }
+    row_populated.push_back(line != empty_row);
     for (int i = 0; i < size; ++i) {
-      column_full[i] = column_full[i] || (line[i] != empty_space);
+      column_populated[i] = column_populated[i] || (line[i] != empty_space);
     }
     space_rows.push_back(std::move(line));
   };
   readfile_op(filename, read_values);
 
-  auto space = expand_columns(space_rows, column_full);
+  auto space =
+      expand_space<factor>(space_rows, row_populated, column_populated);
 
-  int sum = sum_distances(space);
-  std::cout << filename << " -> " << sum << std::endl;
+  int_t sum = sum_distances(space);
+  std::cout << filename << " (" << factor << ") -> " << sum << std::endl;
   return sum;
 }
 
 int main() {
   std::cout << "Part 1" << std::endl;
-  AOC_EXPECT_RESULT(374, (solve_case<false>("day11.example")));
-  AOC_EXPECT_RESULT(10228230, (solve_case<false>("day11.input")));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(2, (solve_case<true>("day11.example")));
-  // AOC_EXPECT_RESULT(977, (solve_case<true>("day11.input")));
+  AOC_EXPECT_RESULT(374, (solve_case<2>("day11.example")));
+  AOC_EXPECT_RESULT(10228230, (solve_case<2>("day11.input")));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(1030, (solve_case<10>("day11.example")));
+  AOC_EXPECT_RESULT(8410, (solve_case<100>("day11.example")));
+  AOC_EXPECT_RESULT(82000210, (solve_case<1000000>("day11.example")));
+  AOC_EXPECT_RESULT(447073334102, (solve_case<1000000>("day11.input")));
   AOC_RETURN_CHECK_RESULT();
 }
