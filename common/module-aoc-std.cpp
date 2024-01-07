9be4afdeff6e44e3a413b73df0ca82f54b0c59fa
Peter Žužek <peterzuzek@gmail.com>
2024-01-07 23:50:51 +0100

    Basic support for a custom `import std`
    Works in Clang, but trying to guard it for Clang actually breaks it,
    which is bizzare.

---
+++ common/module-aoc-std.cpp
@@ -0,0 +1,44 @@
+module;
+
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <cassert>
+#include <charconv>
+#include <chrono>
+#include <cmath>
+#include <compare>
+#include <concepts>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <optional>
+#include <ostream>
+#include <ranges>
+#include <set>
+#include <span>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+export module aoc.std;
+
+export namespace std {
+
+using namespace ::std;
+
+}
