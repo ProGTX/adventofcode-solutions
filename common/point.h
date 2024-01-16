8ab28e97c8590e419b6bb47f7d839269101ec3f5
Peter Žužek <peterzuzek@gmail.com>
2024-01-16 18:39:36 +0000

    Improved calculate_area

--- common/point.h
+++ common/point.h
@@ -249,12 +249,12 @@ constexpr auto calculate_area(std::span<const point_type<T>> polygon) {
       std::conditional_t<std::same_as<return_t, void>, int, return_t>;
   const auto n = polygon.size();
   AOC_ASSERT(n >= 3, "Formula only works on triangles or higher");
-  // First add the edges
-  int area = polygon[0].x * polygon[1].y - polygon[n - 1].x * polygon[n - 2].y +
-             polygon[n - 1].x * polygon[0].y - polygon[0].x * polygon[n - 1].y;
-  for (int i = 1; i < (n - 1); ++i) {
-    area += polygon[i].x * (polygon[i + 1].y - polygon[i - 1].y);
+  actual_ret_t area{};
+  for (int i = 0; i < (n - 1); ++i) {
+    area += polygon[i].x * polygon[i + 1].y - polygon[i + 1].x * polygon[i].y;
   }
+  // Add the last edge
+  area += polygon[n - 1].x * polygon[0].y - polygon[0].x * polygon[n - 1].y;
   return abs(area) / actual_ret_t{2};
 }
 template <class return_t = void, class T>
