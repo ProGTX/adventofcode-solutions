c130c204346b8a560aacb7287ee4fd3e4e6b8fe5
Peter Žužek <peterzuzek@gmail.com>
2024-01-19 20:09:16 +0100

    2023 day 21 part 2 changes
    Doesn't actually work, requires a geometric approach

--- 2023/day21.cpp
+++ 2023/day21.cpp
@@ -4,54 +4,66 @@
 
 #include <algorithm>
 #include <array>
+#include <functional>
 #include <iostream>
 #include <ostream>
 #include <ranges>
-#include <set>
 #include <string>
 #include <string_view>
 #include <vector>
 
-using int_t = int;
+using int_t = std::uint64_t;
 using garden_t = aoc::char_grid<>;
+using positions_t = std::vector<point>;
 
 constexpr inline auto plot = '.';
 constexpr inline auto rock = '#';
 
-std::set<point> move_one(const garden_t& garden,
-                         const std::set<point>& starting) {
-  std::set<point> ending;
+template <bool infinite>
+positions_t move_one(const garden_t& garden, const positions_t& starting) {
+  positions_t ending;
+  const auto bounds = point(garden.num_columns(), garden.num_rows());
   for (auto start : starting) {
     for (auto diff : aoc::basic_neighbor_diffs) {
       point neighbor = start + diff;
-      if (garden.in_bounds(neighbor.y, neighbor.x) &&
-          (garden.at(neighbor.y, neighbor.x) == plot)) {
-        ending.insert(neighbor);
+      if constexpr (!infinite) {
+        if (garden.in_bounds(neighbor.y, neighbor.x) &&
+            (garden.at(neighbor.y, neighbor.x) == plot)) {
+          ending.push_back(neighbor);
+        }
+      } else {
+        auto neighbor_in_bounds = ((neighbor % bounds) + bounds) % bounds;
+        if (garden.at(neighbor_in_bounds.y, neighbor_in_bounds.x) == plot) {
+          ending.push_back(neighbor);
+        }
       }
     }
   }
+  std::ranges::sort(ending, std::less<>{});
+  auto [non_unique_begin, non_unique_end] = std::ranges::unique(ending);
+  ending.resize(std::distance(std::begin(ending), non_unique_begin));
   return ending;
 }
 
-template <int num_steps>
+template <int num_steps, bool infinite>
 int_t solve_case(const std::string& filename) {
-  std::cout << filename << std::endl;
+  std::cout << filename << " " << num_steps << " " << infinite << std::endl;
 
   garden_t garden;
-  std::set<point> positions;
+  positions_t positions;
 
   for (std::string_view line : aoc::views::read_lines(filename)) {
     garden.add_row(line);
     auto start_pos = line.find("S");
     if (start_pos != std::string::npos) {
       auto start = point(start_pos, garden.num_rows() - 1);
-      positions.insert(start);
+      positions.push_back(start);
       garden.modify(plot, start.y, start.x);
     }
   }
 
   for (int i = 0; i < num_steps; ++i) {
-    positions = move_one(garden, positions);
+    positions = move_one<infinite>(garden, positions);
   }
 
   int_t sum = positions.size();
@@ -61,10 +73,17 @@ int_t solve_case(const std::string& filename) {
 
 int main() {
   std::cout << "Part 1" << std::endl;
-  AOC_EXPECT_RESULT(16, (solve_case<6>("day21.example")));
-  AOC_EXPECT_RESULT(3646, (solve_case<64>("day21.input")));
+  AOC_EXPECT_RESULT(16, (solve_case<6, false>("day21.example")));
+  AOC_EXPECT_RESULT(3646, (solve_case<64, false>("day21.input")));
   // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(952408144115, (solve_case<true>("day21.example")));
-  // AOC_EXPECT_RESULT(90111113594927, (solve_case<true>("day21.input")));
+  AOC_EXPECT_RESULT(16, (solve_case<6, true>("day21.example")));
+  AOC_EXPECT_RESULT(50, (solve_case<10, true>("day21.example")));
+  AOC_EXPECT_RESULT(1594, (solve_case<50, true>("day21.example")));
+  AOC_EXPECT_RESULT(6536, (solve_case<100, true>("day21.example")));
+  AOC_EXPECT_RESULT(167004, (solve_case<500, true>("day21.example")));
+  AOC_EXPECT_RESULT(668697, (solve_case<1000, true>("day21.example")));
+  // AOC_EXPECT_RESULT(16733044, (solve_case<5000, true>("day21.example")));
+  // AOC_EXPECT_RESULT(90111113594927, (solve_case<26501365,
+  // true>("day21.input")));
   AOC_RETURN_CHECK_RESULT();
 }
