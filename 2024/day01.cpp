050f1025548231761db7d13c678ce87703dbe493
Peter Žužek <peterzuzek@gmail.com>
2024-12-06 20:04:32 +0000

    2024 day 1 part 2

--- 2024/day01.cpp
+++ 2024/day01.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <ranges>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -16,7 +17,7 @@ constexpr int sum_diffs(std::vector<int> left_list,
   std::ranges::sort(right_list);
 
   int sum = 0;
-  for (int i = 0; i < left_list.size(); i++) {
+  for (int i = 0; i < left_list.size(); ++i) {
     sum += aoc::abs(right_list[i] - left_list[i]);
   }
   return sum;
@@ -25,7 +26,52 @@ constexpr int sum_diffs(std::vector<int> left_list,
 static_assert(sum_diffs({2}, {7}) == 5);
 static_assert(sum_diffs({3, 4, 2, 1, 3, 3}, {4, 3, 5, 3, 9, 3}) == 11);
 
-template <bool words>
+constexpr int similarity_score(std::vector<int> left_list,
+                               std::vector<int> right_list) {
+  std::ranges::sort(left_list);
+  std::ranges::sort(right_list);
+
+  int previous = 0;
+  int multiplier = 0;
+  auto right_start_it = std::begin(right_list);
+  const auto right_end_it = std::end(right_list);
+
+  int sum = 0;
+  for (int i = 0; i < left_list.size(); ++i) {
+    const auto current = left_list[i];
+    if (current == previous) {
+      sum += current * multiplier;
+      continue;
+    }
+
+    auto match_it = std::ranges::find(right_start_it, right_end_it, current);
+    multiplier = 0;
+    while (match_it != right_end_it) {
+      if (*match_it != current) {
+        break;
+      }
+      ++multiplier;
+      ++match_it;
+      right_start_it = match_it;
+    }
+
+    sum += current * multiplier;
+    previous = current;
+  }
+
+  return sum;
+}
+
+static_assert(similarity_score({3}, {4, 3, 5, 3, 9, 3}) == 9);
+static_assert(similarity_score({3, 3}, {4, 3, 5, 3, 9, 3}) == 18);
+static_assert(similarity_score({3, 4}, {4, 3, 5, 3, 9, 3}) == 13);
+static_assert(similarity_score({3, 4, 3}, {4, 3, 5, 3, 9, 3}) == 22);
+static_assert(similarity_score({3, 4, 2}, {4, 3, 5, 3, 9, 3}) == 13);
+static_assert(similarity_score({3, 4, 2, 1}, {4, 3, 5, 3, 9, 3}) == 13);
+static_assert(similarity_score({3, 4, 2, 1, 3}, {4, 3, 5, 3, 9, 3}) == 22);
+static_assert(similarity_score({3, 4, 2, 1, 3, 3}, {4, 3, 5, 3, 9, 3}) == 31);
+
+template <bool similarity>
 int solve_case(const std::string& filename) {
   std::vector<int> left_list;
   std::vector<int> right_list;
@@ -37,7 +83,12 @@ int solve_case(const std::string& filename) {
     right_list.push_back(right);
   }
 
-  auto sum = sum_diffs(left_list, right_list);
+  int sum = 0;
+  if constexpr (similarity) {
+    sum = similarity_score(left_list, right_list);
+  } else {
+    sum = sum_diffs(left_list, right_list);
+  }
 
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
@@ -47,8 +98,8 @@ int main() {
   std::cout << "Part 1" << std::endl;
   AOC_EXPECT_RESULT(11, solve_case<false>("day01.example"));
   AOC_EXPECT_RESULT(3569916, solve_case<false>("day01.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day01.example2"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day01.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(31, solve_case<true>("day01.example"));
+  AOC_EXPECT_RESULT(26407426, solve_case<true>("day01.input"));
   AOC_RETURN_CHECK_RESULT();
 }
