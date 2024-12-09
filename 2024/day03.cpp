33b04e9a0eef474732f6a68071871147d7966bd3
Peter Žužek <peterzuzek@gmail.com>
2024-12-09 18:05:59 +0000

    2024 day 3 part 2 partial

--- 2024/day03.cpp
+++ 2024/day03.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <utility>
 
 static constexpr int max_digits = 3;
 
@@ -74,14 +75,59 @@ constexpr int parse_multiplications(std::string_view line) {
   return sum;
 }
 
-static_assert(parse_multiplications("mul(44,46)") == 2024);
+constexpr int parse_multiplications_with_enablers(std::string_view line) {
+  constexpr auto npos = std::string_view::npos;
+  using pos_t = std::remove_const_t<decltype(npos)>;
+  constexpr std::string_view do_str = "do()";
+  constexpr std::string_view dont_str = "don't()";
+  pos_t current_index = 0;
+  bool mul_enabled = true;
+  int sum = 0;
+  while (true) {
+    if (mul_enabled) {
+      auto index = line.find(dont_str, current_index);
+      if (index == npos) {
+        auto partial = parse_multiplications(line.substr(current_index));
+        sum += partial;
+        break;
+      } else {
+        auto partial = parse_multiplications(
+            line.substr(current_index, index - current_index));
+        sum += partial;
+        mul_enabled = false;
+        current_index = index + dont_str.size();
+      }
+    } else {
+      auto index = line.find(do_str, current_index);
+      if (index == npos) {
+        break;
+      } else {
+        mul_enabled = true;
+        current_index = index + do_str.size();
+      }
+    }
+  }
+  return sum;
+}
 
-template <bool>
+static_assert(2024 == parse_multiplications("mul(44,46)"));
+static_assert(33 ==
+              parse_multiplications("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)"));
+static_assert(8 == parse_multiplications_with_enablers(
+                       "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)"));
+static_assert(1019406 == parse_multiplications(
+                             "[#from())when()/}+%mul(982,733)mul(700,428)}}"));
+
+template <bool enablers>
 int solve_case(const std::string& filename) {
   int sum = 0;
 
   for (std::string_view line : aoc::views::read_lines(filename)) {
-    sum += parse_multiplications(line);
+    if constexpr (enablers) {
+      sum += parse_multiplications_with_enablers(line);
+    } else {
+      sum += parse_multiplications(line);
+    }
   }
 
   std::cout << filename << " -> " << sum << std::endl;
@@ -92,8 +138,8 @@ int main() {
   std::cout << "Part 1" << std::endl;
   AOC_EXPECT_RESULT(161, solve_case<false>("day03.example"));
   AOC_EXPECT_RESULT(174960292, solve_case<false>("day03.input"));
-  //   std::cout << "Part 2" << std::endl;
-  //   AOC_EXPECT_RESULT(281, solve_case<true>("day03.example"));
-  //   AOC_EXPECT_RESULT(53515, solve_case<true>("day03.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(48, solve_case<true>("day03.example2"));
+  AOC_EXPECT_RESULT(-61636489, solve_case<true>("day03.input"));
   AOC_RETURN_CHECK_RESULT();
 }
