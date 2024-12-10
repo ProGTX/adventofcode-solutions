d59cf7c13c04b1c4998b3763e8f2ccdb89d8e27e
Peter Žužek <peterzuzek@gmail.com>
2024-12-10 11:15:14 +0000

    2024 day 4 part 2

--- 2024/day04.cpp
+++ 2024/day04.cpp
@@ -13,6 +13,10 @@ using word_board_t = aoc::char_grid<>;
 
 // Empty character used both as separator and to signal out of bounds
 static constexpr char empty_char = '?';
+
+////////////////////////////////////////////////////////////////////////////////
+// PART 1
+
 static constexpr auto xmas_str = "XMAS"sv;
 static constexpr auto xmas_rstr = "SAMX"sv;
 
@@ -117,7 +121,99 @@ constexpr int count_xmas_board(const word_board_t& board) {
          count_xmas_diagonal(board);
 }
 
-template <bool>
+////////////////////////////////////////////////////////////////////////////////
+// PART 2
+
+constexpr void set_space(char& c) {
+  if (c != empty_char) {
+    c = ' ';
+  }
+}
+
+static constexpr std::size_t normalized_str_size = 9;
+
+constexpr std::string normalize_local_grid(std::string_view row1,
+                                           std::string_view row2,
+                                           std::string_view row3) {
+  AOC_ASSERT(row1.size() == 3, "All three rows must be size 3");
+  AOC_ASSERT(row1.size() == row2.size(), "All three rows must be size 3");
+  AOC_ASSERT(row2.size() == row3.size(), "All three rows must be size 3");
+  std::string result;
+  result.reserve(normalized_str_size);
+  result += row1;
+  result += row2;
+  result += row3;
+  set_space(result[1]);
+  set_space(result[3]);
+  set_space(result[5]);
+  set_space(result[7]);
+  return result;
+}
+
+static constexpr auto x_mas_string_storage = []() {
+  std::array<std::array<char, normalized_str_size>, 4> result;
+  std::ranges::copy(normalize_local_grid("M.S", //
+                                         ".A.", //
+                                         "M.S"  //
+                                         ),
+                    std::begin(result[0]));
+  std::ranges::copy(normalize_local_grid("M.M", //
+                                         ".A.", //
+                                         "S.S"  //
+                                         ),
+                    std::begin(result[1]));
+  std::ranges::copy(normalize_local_grid("S.S", //
+                                         ".A.", //
+                                         "M.M"  //
+                                         ),
+                    std::begin(result[2]));
+  std::ranges::copy(normalize_local_grid("S.M", //
+                                         ".A.", //
+                                         "S.M"  //
+                                         ),
+                    std::begin(result[3]));
+  return result;
+}();
+
+static constexpr std::array x_mas_strings = {
+    std::string_view{x_mas_string_storage[0].data(), normalized_str_size},
+    std::string_view{x_mas_string_storage[1].data(), normalized_str_size},
+    std::string_view{x_mas_string_storage[2].data(), normalized_str_size},
+    std::string_view{x_mas_string_storage[3].data(), normalized_str_size},
+};
+
+static_assert("M S A M S" == x_mas_strings[0]);
+
+// The algorithm is to find each A, collect the surrounding area,
+// transform that into a string, and compare against all valid x_mas strings
+static constexpr int count_x_mas_crosses(const word_board_t& board) {
+  const auto get_substr = [&](int row, int column) {
+    return std::string_view{&board.at(row, column), 3};
+  };
+  int count = 0;
+  // Go through the board, but skip edges to avoid bound checking
+  for (int row = 1; row < board.num_rows() - 1; ++row) {
+    auto it = board.begin_row(row);
+    ++it;
+    for (int col = 1; col < board.num_columns() - 1; ++col, ++it) {
+      if (*it != 'A') {
+        continue;
+      }
+      // Collect area around the A
+      auto area_str = normalize_local_grid(get_substr(row - 1, col - 1),
+                                           get_substr(row, col - 1),
+                                           get_substr(row + 1, col - 1));
+      count += static_cast<int>(
+          aoc::ranges::contains(x_mas_strings, std::string_view{area_str}));
+    }
+  }
+  return count;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// MAIN
+
+template <bool x_mas>
 int solve_case(const std::string& filename) {
   word_board_t board;
 
@@ -126,7 +222,12 @@ int solve_case(const std::string& filename) {
   }
   board.add_row(aoc::views::repeat(empty_char, board.row_length()));
 
-  auto sum = count_xmas_board(board);
+  int sum = 0;
+  if constexpr (x_mas) {
+    sum = count_x_mas_crosses(board);
+  } else {
+    sum = count_xmas_board(board);
+  }
 
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
@@ -137,8 +238,8 @@ int main() {
   AOC_EXPECT_RESULT(4, solve_case<false>("day04.example2"));
   AOC_EXPECT_RESULT(18, solve_case<false>("day04.example"));
   AOC_EXPECT_RESULT(2517, solve_case<false>("day04.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(48, solve_case<true>("day04.example2"));
-  // AOC_EXPECT_RESULT(-61636489, solve_case<true>("day04.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(9, solve_case<true>("day04.example"));
+  AOC_EXPECT_RESULT(1960, solve_case<true>("day04.input"));
   AOC_RETURN_CHECK_RESULT();
 }
