6d34b2042db7ba2243921686bcbdf2b4ae33655f
Peter Žužek <peterzuzek@gmail.com>
2024-12-11 09:38:22 +0000

    2024 day 5 part 2

--- 2024/day05.cpp
+++ 2024/day05.cpp
@@ -4,6 +4,8 @@
 
 #include <array>
 #include <iostream>
+#include <iterator>
+#include <set>
 #include <string>
 #include <string_view>
 
@@ -41,7 +43,24 @@ constexpr int index_of_correct_update(const std::vector<ordering_rule_t>& rules,
   return (update.size() / 2);
 }
 
-template <bool>
+// Orders the update and returns the middle element
+int order_update(const std::vector<ordering_rule_t>& rules,
+                 std::span<const int> update) {
+  AOC_ASSERT(rules.size() >= 100,
+             "Vector of rules used as a hashmap, "
+             "so it needs to have enough space to store all pages");
+  const auto comparator = [&](int lhs, int rhs) {
+    return aoc::ranges::contains(rules[rhs].before, lhs);
+  };
+  std::set<int, decltype(comparator)> ordered_update{comparator};
+  std::ranges::copy(update,
+                    std::inserter(ordered_update, ordered_update.end()));
+  auto it = std::begin(ordered_update);
+  std::advance(it, ordered_update.size() / 2);
+  return *it;
+}
+
+template <bool order>
 int solve_case(const std::string& filename) {
   int sum = 0;
 
@@ -64,8 +83,12 @@ int solve_case(const std::string& filename) {
     } else {
       auto update = aoc::split<update_t>(line, ',');
       auto index = index_of_correct_update(rules, update);
-      if (index > 0) {
-        sum += update[index];
+      if constexpr (!order) {
+        if (index >= 0) {
+          sum += update[index];
+        }
+      } else if (index < 0) {
+        sum += order_update(rules, update);
       }
     }
   }
@@ -80,8 +103,10 @@ int main() {
   AOC_EXPECT_RESULT(0, solve_case<false>("day05.example3"));
   AOC_EXPECT_RESULT(143, solve_case<false>("day05.example"));
   AOC_EXPECT_RESULT(6034, solve_case<false>("day05.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day05.example"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day05.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(0, solve_case<true>("day05.example2"));
+  AOC_EXPECT_RESULT(47, solve_case<true>("day05.example3"));
+  AOC_EXPECT_RESULT(123, solve_case<true>("day05.example"));
+  AOC_EXPECT_RESULT(6305, solve_case<true>("day05.input"));
   AOC_RETURN_CHECK_RESULT();
 }
