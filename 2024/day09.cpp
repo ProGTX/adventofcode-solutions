10c8bc43d16fd44b9c84ed4ac18f4647a64390a8
Peter Žužek <peterzuzek@gmail.com>
2024-12-18 18:53:41 +0000

    2024 day 9 part 2

--- 2024/day09.cpp
+++ 2024/day09.cpp
@@ -65,6 +65,63 @@ constexpr filesystem_t compact(filesystem_t filesystem) {
   return filesystem;
 }
 
+constexpr filesystem_t compact_nonfragment(filesystem_t filesystem) {
+  // 9 because that's the highest number possible
+  std::array<int, 9> max_empty_range;
+  std::ranges::fill(max_empty_range, empty_space);
+  const auto max_empty_range_begin = max_empty_range.begin();
+  using iterator = filesystem_t::iterator;
+  const auto begin = filesystem.begin();
+  const auto end = filesystem.end();
+  const auto rend = filesystem.rend();
+  iterator it = end;
+  for (auto rit = filesystem.rbegin(); rit != rend; ++rit) {
+    // Skip over empty spaces from the back
+    while (*rit == empty_space) {
+      ++rit;
+      if (rit == rend) {
+        break;
+      }
+    }
+    if (rit == rend) {
+      break;
+    }
+    // Collect current elements into a range
+    const auto current = *rit;
+    const auto current_range_end = rit.base();
+    auto current_range_begin = current_range_end;
+    while (current_range_begin != begin) {
+      auto next_it = current_range_begin - 1;
+      if (*next_it != current) {
+        break;
+      }
+      current_range_begin = next_it;
+    }
+
+    // Progress the reverse iterator to skip the current range
+    // Plus 1 because a reverse iterator points to the element
+    // before the forward iterator
+    rit = std::reverse_iterator(current_range_begin + 1);
+
+    auto current_range =
+        std::ranges::subrange(current_range_begin, current_range_end);
+    const auto current_size = current_range.size();
+
+    // Find an empty range that can fit the current range
+    it = std::search(begin, current_range_begin, max_empty_range_begin,
+                     max_empty_range_begin + current_size);
+    if ((it == end) || (it >= current_range_begin)) {
+      // Can't fit the current range in any free space to the left
+      continue;
+    }
+
+    // Swap the current range with the empty space
+    std::ranges::swap_ranges(std::ranges::subrange(it, it + current_size),
+                             current_range);
+  }
+  return filesystem;
+}
+
 constexpr int_t checksum(std::span<const int> filesystem) {
   return aoc::ranges::accumulate(filesystem | aoc::views::enumerate() |
                                      std::views::transform([](auto&& current) {
@@ -89,20 +146,26 @@ static_assert(1928 ==
               checksum(std::array{0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7,
                                   3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6, -1}));
 
+template <bool nonfragment>
 constexpr int_t compact_from_disk_map(std::string_view disk_map) {
-  return checksum(compact(expand(disk_map)));
+  auto filesystem = expand(disk_map);
+  if constexpr (!nonfragment) {
+    return checksum(compact(std::move(filesystem)));
+  } else {
+    return checksum(compact_nonfragment(std::move(filesystem)));
+  }
 }
 
-static_assert(60 == compact_from_disk_map("12345"));
+static_assert(60 == compact_from_disk_map<false>("12345"));
 
-template <bool>
+template <bool nonfragment>
 int_t solve_case(const std::string& filename) {
   std::string disk_map;
   for (std::string line : aoc::views::read_lines(filename)) {
     disk_map = std::move(line);
     break;
   }
-  auto sum = compact_from_disk_map(disk_map);
+  auto sum = compact_from_disk_map<nonfragment>(disk_map);
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
 }
@@ -111,8 +174,8 @@ int main() {
   std::cout << "Part 1" << std::endl;
   AOC_EXPECT_RESULT(1928, solve_case<false>("day09.example"));
   AOC_EXPECT_RESULT(6432869891895, solve_case<false>("day09.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day09.example"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day09.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(2858, solve_case<true>("day09.example"));
+  AOC_EXPECT_RESULT(6467290479134, solve_case<true>("day09.input"));
   AOC_RETURN_CHECK_RESULT();
 }
