978a943dd10016d07ff2dc5b65fbe1a81ce4627a
Peter Žužek <peterzuzek@gmail.com>
2024-12-30 13:03:51 +0100

    2024 day 13 part 2

--- 2024/day13.cpp
+++ 2024/day13.cpp
@@ -3,18 +3,22 @@
 #include "../common/common.h"
 
 #include <array>
+#include <cstdint>
 #include <iostream>
 #include <ranges>
 #include <string>
 #include <string_view>
 #include <vector>
 
-struct claw_machine {
-  point a;
-  point b;
-  point prize;
+using int_t = std::int64_t;
+using point_t = aoc::point_type<int_t>;
 
-  constexpr int lowest_cost() {
+struct claw_machine {
+  point_t a;
+  point_t b;
+  point_t prize;
+
+  constexpr int_t lowest_cost() const {
     // prize = c
     // A*ax + B*bx = cx
     // A*ay + B*by = cy
@@ -26,7 +30,14 @@ struct claw_machine {
       return 0;
     }
     auto B = B_num / B_den;
-    auto A = (prize.y - B * b.y) / a.y;
+
+    auto A_num = (prize.y - B * b.y);
+    auto A_den = a.y;
+    if ((A_den == 0) || ((A_num % A_den) != 0)) {
+      return 0;
+    }
+    auto A = A_num / A_den;
+
     return A * 3 + B;
   }
 };
@@ -40,12 +51,31 @@ static_assert(200 ==
 static_assert(0 ==
               claw_machine{{69, 23}, {27, 71}, {18641, 10279}}.lowest_cost());
 
-constexpr std::string_view prize_prefix = "Prize: ";
-constexpr std::string_view ax_prefix = "A: X+";
-constexpr std::string_view y_prefix = " Y+";
+constexpr const int_t correction = 10000000000000;
 
-template <bool>
-int solve_case(const std::string& filename) {
+static_assert(0 == claw_machine{{94, 34},
+                                {22, 67},
+                                {correction + 8400, correction + 5400}}
+                       .lowest_cost());
+static_assert(459236326669 ==
+              claw_machine{
+                  {26, 66}, {67, 21}, {correction + 12748, correction + 12176}}
+                  .lowest_cost());
+static_assert(0 == claw_machine{{17, 86},
+                                {84, 37},
+                                {correction + 7870, correction + 6450}}
+                       .lowest_cost());
+static_assert(416082282239 ==
+              claw_machine{
+                  {69, 23}, {27, 71}, {correction + 18641, correction + 10279}}
+                  .lowest_cost());
+
+constexpr const std::string_view prize_prefix = "Prize: ";
+constexpr const std::string_view ax_prefix = "A: X+";
+constexpr const std::string_view y_prefix = " Y+";
+
+template <bool correct_conversion>
+int_t solve_case(const std::string& filename) {
   std::vector<claw_machine> machines;
   machines.emplace_back();
 
@@ -62,22 +92,25 @@ int solve_case(const std::string& filename) {
     auto [x, y] = aoc::split<std::array<std::string_view, 2>>(line, ',');
 
     if (prefix == prize_prefix) {
-      current.prize = point{aoc::to_number<int>(x.substr(2)),
-                            aoc::to_number<int>(y.substr(y_prefix.size()))};
+      current.prize = point_t{aoc::to_number<int_t>(x.substr(2)),
+                              aoc::to_number<int_t>(y.substr(y_prefix.size()))};
+      if constexpr (correct_conversion) {
+        current.prize += point_t{correction, correction};
+      }
     } else if (line[0] == 'A') {
-      current.a = point{aoc::to_number<int>(x.substr(ax_prefix.size())),
-                        aoc::to_number<int>(y.substr(y_prefix.size()))};
+      current.a = point_t{aoc::to_number<int_t>(x.substr(ax_prefix.size())),
+                          aoc::to_number<int_t>(y.substr(y_prefix.size()))};
     } else if (line[0] == 'B') {
-      current.b = point{aoc::to_number<int>(x.substr(ax_prefix.size())),
-                        aoc::to_number<int>(y.substr(y_prefix.size()))};
+      current.b = point_t{aoc::to_number<int_t>(x.substr(ax_prefix.size())),
+                          aoc::to_number<int_t>(y.substr(y_prefix.size()))};
     } else {
       AOC_ASSERT(false, "Parsing failure");
     }
   }
 
-  int sum = 0;
+  int_t sum = 0;
   sum = aoc::ranges::accumulate(
-      machines | std::views::transform(&claw_machine::lowest_cost), 0);
+      machines | std::views::transform(&claw_machine::lowest_cost), int_t{0});
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
 }
@@ -86,8 +119,9 @@ int main() {
   std::cout << "Part 1" << std::endl;
   AOC_EXPECT_RESULT(480, solve_case<false>("day13.example"));
   AOC_EXPECT_RESULT(38714, solve_case<false>("day13.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day13.example"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day13.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT((459236326669 + 416082282239),
+                    solve_case<true>("day13.example"));
+  AOC_EXPECT_RESULT(74015623345775, solve_case<true>("day13.input"));
   AOC_RETURN_CHECK_RESULT();
 }
