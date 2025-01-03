ae46b52beceb8c8c7691269354a741d9ff3823cc
Peter Žužek <peterzuzek@gmail.com>
2025-01-03 12:59:16 +0000

    2024 day 16 part 2
    Well, kinda. It's correct for the small example and for the main input,
    but fails on the larger example, so there's still an issue somewhere.

--- 2024/day16.cpp
+++ 2024/day16.cpp
@@ -13,17 +13,13 @@ constexpr const char end = 'E';
 
 using maze_t = aoc::char_grid<>;
 using arrow_t = aoc::arrow_type<int>;
+using predecessors_t = aoc::predecessor_map<arrow_t>;
 
-constexpr int lowest_score(const maze_t& maze, point start_pos, point end_pos) {
-  const auto end_arrows =
-      std::array{aoc::east, aoc::north} |
-      std::views::transform([&](const aoc::facing_t facing) {
-        return arrow_t{end_pos, facing};
-      }) |
-      aoc::ranges::to<aoc::static_vector<arrow_t, 2>>();
-
+constexpr auto get_distances(const maze_t& maze, point start_pos,
+                             std::span<const arrow_t> end_arrows,
+                             predecessors_t* predecessors) {
   auto distances = aoc::shortest_distances_dijkstra(
-      arrow_t{start_pos, aoc::east}, std::span{end_arrows},
+      arrow_t{start_pos, aoc::east}, end_arrows,
       [&](const arrow_t current) {
         auto neighbors =
             aoc::static_vector<aoc::dijkstra_neighbor_t<arrow_t>, 3>{};
@@ -48,7 +44,23 @@ constexpr int lowest_score(const maze_t& maze, point start_pos, point end_pos) {
         neighbors.emplace_back(arrow_t{current.position, new_direction}, 1000);
 
         return neighbors;
-      });
+      },
+      predecessors);
+
+  return distances;
+}
+
+constexpr auto get_end_arrows(point end_pos) {
+  return std::array{aoc::east, aoc::north} |
+         std::views::transform([&](const aoc::facing_t facing) {
+           return arrow_t{end_pos, facing};
+         }) |
+         aoc::ranges::to<aoc::static_vector<arrow_t, 2>>();
+}
+
+constexpr int lowest_score(const maze_t& maze, point start_pos, point end_pos) {
+  const auto end_arrows = get_end_arrows(end_pos);
+  auto distances = get_distances(maze, start_pos, end_arrows, nullptr);
 
   int lowest = 1 << 30;
   for (auto end_arrow : end_arrows) {
@@ -60,7 +72,72 @@ constexpr int lowest_score(const maze_t& maze, point start_pos, point end_pos) {
   return lowest;
 }
 
-template <bool>
+constexpr void add_tiles(const maze_t& maze, const auto& distances,
+                         const predecessors_t& predecessors,
+                         aoc::flat_set<point>& tiles, arrow_t current_arrow) {
+
+  const auto try_explore_neighbor = [&](arrow_t neighbor_arrow,
+                                        const arrow_t pred_arrow,
+                                        const int pred_distance) {
+    if (neighbor_arrow == pred_arrow) {
+      return;
+    }
+    const auto distance_it = distances.find(neighbor_arrow);
+    if ((distance_it != std::end(distances)) &&
+        (distance_it->second == pred_distance)) {
+      add_tiles(maze, distances, predecessors, tiles, neighbor_arrow);
+    }
+  };
+
+  while (true) {
+    const auto current_pos = current_arrow.position;
+    tiles.insert(current_pos);
+    auto pred_it = predecessors.find(current_arrow);
+    if (pred_it == std::end(predecessors)) {
+      return;
+    }
+    const auto current_distance = distances.at(current_arrow);
+    // Explore other potential predecessors that have the same score
+    // This is done in reverse of the original neighbor search
+
+    {
+      const auto neighbor_pos =
+          current_pos - aoc::get_diff(current_arrow.direction);
+      // Note we don't care for checking the starting node here
+      // because that's guaranteed to be on the shortest path
+      if (maze.at(neighbor_pos.y, neighbor_pos.x) == empty) {
+        try_explore_neighbor(arrow_t{neighbor_pos, current_arrow.direction},
+                             pred_it->second, current_distance - 1);
+      }
+    }
+    try_explore_neighbor(
+        arrow_t{current_pos, aoc::anticlockwise_basic(current_arrow.direction)},
+        pred_it->second, current_distance - 1000);
+    try_explore_neighbor(
+        arrow_t{current_pos, aoc::clockwise_basic(current_arrow.direction)},
+        pred_it->second, current_distance - 1000);
+
+    current_arrow = pred_it->second;
+  }
+}
+
+constexpr int tiles_on_best_paths(const maze_t& maze, point start_pos,
+                                  point end_pos) {
+  // In this case we don't want to terminate the search
+  // when finding the best path, so we don't pass any end arrows
+  std::array<arrow_t, 0> fake_end_arrows;
+  predecessors_t predecessors;
+  auto distances =
+      get_distances(maze, start_pos, fake_end_arrows, &predecessors);
+
+  aoc::flat_set<point> tiles;
+  for (const auto end_arrow : get_end_arrows(end_pos)) {
+    add_tiles(maze, distances, predecessors, tiles, end_arrow);
+  }
+  return tiles.size();
+}
+
+template <bool tiles>
 int solve_case(const std::string& filename) {
   maze_t maze;
   point start_pos;
@@ -76,7 +153,11 @@ int solve_case(const std::string& filename) {
   }
 
   int sum = 0;
-  sum = lowest_score(maze, start_pos, end_pos);
+  if constexpr (!tiles) {
+    sum = lowest_score(maze, start_pos, end_pos);
+  } else {
+    sum = tiles_on_best_paths(maze, start_pos, end_pos);
+  }
   std::cout << filename << " -> " << sum << std::endl;
   return sum;
 }
@@ -90,8 +171,9 @@ int main() {
   // https://www.reddit.com/r/adventofcode/comments/1hgyuqm/2024_day_16_part_1/
   AOC_EXPECT_RESULT(5027, solve_case<false>("day16.example4"));
   AOC_EXPECT_RESULT(94436, solve_case<false>("day16.input"));
-  // std::cout << "Part 2" << std::endl;
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day16.example"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day16.input"));
+  std::cout << "Part 2" << std::endl;
+  AOC_EXPECT_RESULT(45, solve_case<true>("day16.example"));
+  // AOC_EXPECT_RESULT(64, solve_case<true>("day16.example2"));
+  AOC_EXPECT_RESULT(481, solve_case<true>("day16.input"));
   AOC_RETURN_CHECK_RESULT();
 }
