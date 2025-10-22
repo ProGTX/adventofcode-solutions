e382041691388d727a91642433b3740370c7cc98
Peter Žužek <peterzuzek@gmail.com>
2025-10-22 22:02:20 +0200

    Initial try of a Rust version of 2015 day 1

--- rust/2015/src/day01.rs
+++ rust/2015/src/day01.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+fn solve_case(filename: &str) -> io::Result<i32> {
+    let mut file = File::open(filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let floor = contents
+        .chars()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => 0,
+        })
+        .sum();
+
+    Ok(floor)
+}
+
 fn main() {
     println!("Hello world!");
+    let example_result = solve_case("day01.example");
+    assert_eq!(example_result, 3);
 }
