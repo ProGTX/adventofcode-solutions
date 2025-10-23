f16f5bad3f4284b3186d728e3ab505d60c60a063
Peter Žužek <peterzuzek@gmail.com>
2025-10-23 11:59:10 +0200

    Start using std::print

--- 2015/day01.cpp
+++ 2015/day01.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <iostream>
+#include <print>
 #include <string>
 
 template <bool check_position>
@@ -24,10 +25,10 @@ int solve_case(const std::string& filename) {
 }
 
 int main() {
-  std::cout << "Part 1" << std::endl;
+  std::println("Part 1");
   AOC_EXPECT_RESULT(-1, solve_case<false>("day01.example"));
   AOC_EXPECT_RESULT(280, solve_case<false>("day01.input"));
-  std::cout << "Part 2" << std::endl;
+  std::println("Part 2");
   AOC_EXPECT_RESULT(5, solve_case<true>("day01.example"));
   AOC_EXPECT_RESULT(1797, solve_case<true>("day01.input"));
   AOC_RETURN_CHECK_RESULT();
