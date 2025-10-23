b3a2353276083b702701edebf9a9a281c93399a4
Peter Žužek <peterzuzek@gmail.com>
2025-10-23 11:43:58 +0200

    2015 day 1 part 2 Rust

--- rust/2015/src/day01.rs
+++ rust/2015/src/day01.rs
@@ -1,27 +1,29 @@
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, BufRead, BufReader};
 
-fn solve_case(filename: &str) -> io::Result<i32> {
-    let mut file = File::open(filename)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-
-    let floor = contents
-        .chars()
-        .map(|c| match c {
-            '(' => 1,
-            ')' => -1,
-            _ => 0,
-        })
-        .sum();
-
-    Ok(floor)
+fn solve_case<const CHECK_POSITION: bool>(filename: &str) -> io::Result<i32> {
+    let mut floor = 0;
+    let mut position = 0;
+    let file = File::open(filename)?;
+    let mut line = String::new();
+    BufReader::new(file).read_line(&mut line)?;
+    for c in line.chars() {
+        floor += (c == '(') as i32 - (c == ')') as i32;
+        if CHECK_POSITION {
+            position += 1;
+            if floor == -1 {
+                break;
+            }
+        }
+    }
+    Ok(if CHECK_POSITION { position } else { floor })
 }
 
 fn main() {
     println!("Part 1");
-    let example_result = solve_case("day01.example").unwrap();
-    assert_eq!(example_result, -1);
-    let input_result = solve_case("day01.input").unwrap();
-    assert_eq!(input_result, 280);
+    assert_eq!(-1, solve_case::<false>("day01.example").unwrap());
+    assert_eq!(280, solve_case::<false>("day01.input").unwrap());
+    println!("Part 2");
+    assert_eq!(5, solve_case::<true>("day01.example").unwrap());
+    assert_eq!(1797, solve_case::<true>("day01.input").unwrap());
 }
