0359ab05f27828ecabcd1e43e36bba9eb8678f3a
Peter Žužek <peterzuzek@gmail.com>
2025-10-29 09:31:57 +0100

    2015 day 9
    C++ only
    Solution is much simpler if the graph is fully connected

--- 2015/day09.cpp
+++ 2015/day09.cpp
@@ -19,11 +19,6 @@ using str = std::string_view;
 struct link_t {
   int to_id;
   int distance;
-
-  friend std::ostream& operator<<(std::ostream& out, const link_t& link) {
-    out << std::format("({}, {})", link.to_id, link.distance);
-    return out;
-  }
 };
 
 using connections_t = std::vector<std::vector<link_t>>;
@@ -51,95 +46,57 @@ connections_t parse(const std::string& filename) {
     connections[from_id].emplace_back(to_id, distance);
     connections[to_id].emplace_back(from_id, distance);
   }
-  for (auto& con : connections) {
-    std::ranges::sort(con, std::ranges::less{}, &link_t::distance);
-  }
   return connections;
 }
 
-struct node_t {
-  int from_id;
-  int link_index;
-  link_t link;
-};
-
-constexpr int shortest_distance(const connections_t& connections) {
-  auto shortest = std::numeric_limits<int>::max();
-  auto current_distance = 0;
-  auto current_path = std::vector<node_t>{};
-  let on_path = [&](const int id) {
-    return std::ranges::contains(current_path, id, &node_t::from_id);
-  };
-  let backtrack_path = [&]() {
-    while (!current_path.empty()) {
-      auto& current_place = current_path.back();
-      // Backtrack distance we traveled to here
-      current_distance -= current_place.link.distance;
-      // Find a new destination that we haven't visited yet
-      let& possible_destinations = connections[current_place.from_id];
-      let it = std::ranges::find_if_not(
-          possible_destinations |
-              std::views::take(current_place.link_index + 1),
-          on_path, &link_t::to_id);
-      if (it == std::end(possible_destinations)) {
-        // No destination was OK, try going back one step
-        current_path.pop_back();
-      } else {
-        // Found a new path base
-        current_place.link = *it;
-        break;
-      }
+// This function assumes the graph is fully connected
+template <auto compare>
+constexpr int best_distance(const connections_t& connections) {
+  auto best = []() {
+    if constexpr (compare(0, 1)) { // less
+      return std::numeric_limits<int>::max();
+    } else { // greater
+      return 0;
     }
-  };
-  for (let[from_id, place] : connections | std::views::enumerate) {
+  }();
+  auto current_path = aoc::flat_set<int>{};
+  let on_path = [&](const int id) { return current_path.contains(id); };
+  for (let& [ from_id, place ] : connections | std::views::enumerate) {
+    AOC_ASSERT(std::ranges::is_sorted(place, compare, &link_t::distance),
+               "Destinations must be sorted by distance");
+    auto current_link = place;
     current_path.clear();
-    current_path.emplace_back(from_id, 0, place[0]);
-    current_distance = 0;
-    while (!current_path.empty()) {
-      let& current_place = current_path.back();
-      current_distance += current_place.link.distance;
-      if (current_distance >= shortest) {
-        backtrack_path();
-        continue;
-      }
-      let full_path = current_path.size() == (connections.size() - 1);
-      if (full_path) {
-        if (current_distance < shortest) {
-          shortest = current_distance;
-        }
-        backtrack_path();
-      } else {
-        let& possible_destinations = connections[current_place.link.to_id];
-        let it = std::ranges::find_if_not(possible_destinations, on_path,
-                                          &link_t::to_id);
-        if (it == std::end(possible_destinations)) {
-          // Cannot travel any more
-          break;
-        }
-        let index = std::distance(std::begin(possible_destinations), it);
-        current_path.emplace_back(current_place.link.to_id, index,
-                                  possible_destinations[index]);
-      }
+    current_path.insert(from_id);
+    auto current_distance = 0;
+    while (current_path.size() < connections.size()) {
+      let it = std::ranges::find_if_not(current_link, on_path, &link_t::to_id);
+      AOC_ASSERT(it != std::end(current_link), "Graph must be fully connected");
+      current_path.insert(it->to_id);
+      current_distance += it->distance;
+      current_link = connections[it->to_id];
+    }
+    if (compare(current_distance, best)) {
+      best = current_distance;
     }
   }
-  return shortest;
+  return best;
 }
 
-int solve_case1(const std::string& filename) {
-  let connections = parse(filename);
-  for (let& [ index, connection ] : connections | std::views::enumerate) {
-    std::cout << index << ": " << aoc::print_range(connection) << std::endl;
+template <auto compare>
+int solve_case(const std::string& filename) {
+  auto connections = parse(filename);
+  for (auto& place : connections) {
+    std::ranges::sort(place, compare, &link_t::distance);
   }
-  return 0;
-  // return shortest_distance(connections);
+  return best_distance<compare>(connections);
 }
 
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT(605, solve_case1("day09.example"));
-  AOC_EXPECT_RESULT(1333, solve_case1("day09.input"));
-  // std::println("Part 2");
-  // AOC_EXPECT_RESULT(19, solve_case2("day09.example"));
-  // AOC_EXPECT_RESULT(2046, solve_case2("day09.input"));
+  AOC_EXPECT_RESULT(605, solve_case<std::ranges::less{}>("day09.example"));
+  AOC_EXPECT_RESULT(141, solve_case<std::ranges::less{}>("day09.input"));
+  std::println("Part 2");
+  AOC_EXPECT_RESULT(982, solve_case<std::ranges::greater{}>("day09.example"));
+  AOC_EXPECT_RESULT(736, solve_case<std::ranges::greater{}>("day09.input"));
   AOC_RETURN_CHECK_RESULT();
 }
