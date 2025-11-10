d7451c50a2b9ebe76b76748688728a923546e12d
Peter Žužek <peterzuzek@gmail.com>
2025-11-10 15:26:08 +0100

    2015 day 12 part 1 C++

--- 2015/day12.cpp
+++ 2015/day12.cpp
@@ -4,15 +4,37 @@
 #include "../common/rust.h"
 
 #include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <print>
 #include <ranges>
 
+fn sum(str s_view) -> i32 {
+  auto value = 0;
+  while (!s_view.empty()) {
+    let digit_it =
+        std::ranges::find_if(s_view, [](char c) { return std::isdigit(c); });
+    if (digit_it == std::end(s_view)) {
+      break;
+    }
+    let digit_pos = std::distance(std::begin(s_view), digit_it);
+    AOC_ASSERT(digit_pos > 0, "Assuming number not at beginning of string");
+    let nondigit_it = std::ranges::find_if(
+        s_view.substr(digit_pos + 1), [](char c) { return !std::isdigit(c); });
+    AOC_ASSERT(nondigit_it != std::end(s_view),
+               "Assuming number is never at the end");
+    let nondigit_pos = std::distance(digit_it, nondigit_it) + digit_pos;
+    let current =
+        aoc::to_number<i32>(s_view.substr(digit_pos, nondigit_pos - digit_pos));
+    value += (s_view[digit_pos - 1] == '-') ? -current : current;
+    s_view = s_view.substr(nondigit_pos);
+  }
+  return value;
+}
 
-int solve_case(const String& filename) {
-  std::ifstream file{filename};
-  aoc::read_line(file);
-  return -1;
+fn solve_case(String const& filename) -> i32 {
+  auto file = std::ifstream{filename};
+  return sum(aoc::read_line(file));
 }
 
 int main() {
