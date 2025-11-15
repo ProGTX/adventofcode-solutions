f6f98295a78aa20f9deafa59108f51a90faad840
Peter Žužek <peterzuzek@gmail.com>
2025-11-15 21:52:48 +0100

    2015 day 14 part 2
    C++ and Rust

--- 2015/day14.cpp
+++ 2015/day14.cpp
@@ -28,7 +28,7 @@ fn parse(String const& filename) -> Vec<Reindeer> {
 }
 
 template <u32 SECONDS>
-fn solve_case(String const& filename) -> u32 {
+fn solve_case1(String const& filename) -> u32 {
   return std::ranges::max(
       parse(filename) | std::views::transform([](Reindeer const& reindeer) {
         let cycle_time = reindeer.fly + reindeer.rest;
@@ -40,12 +40,44 @@ fn solve_case(String const& filename) -> u32 {
       }));
 }
 
+template <u32 SECONDS>
+fn solve_case2(String const& filename) -> u32 {
+  let flock = parse(filename);
+  auto distances = std::array<u32, 9>{};
+  auto score = std::array<u32, 9>{};
+  AOC_ASSERT(flock.size() <= score.size(), "Too many reindeer");
+  for (let second : std::views::iota(0u, SECONDS)) {
+    // Move each reindeer
+    for (let[index, reindeer] : flock | std::views::enumerate) {
+      let cycle_time = reindeer.fly + reindeer.rest;
+      let flying = (second % cycle_time) < reindeer.fly;
+      distances[index] += static_cast<u32>(flying) * reindeer.kms;
+    }
+    // Award the farthest reindeer
+    using index_dist_t = std::pair<usize, u32>;
+    auto distances_sorted = distances |
+                            std::views::enumerate |
+                            aoc::ranges::to<std::vector<index_dist_t>>();
+    std::ranges::sort(distances_sorted, std::ranges::greater{},
+                      &index_dist_t::second);
+    let farthest_dist = distances_sorted.front().second;
+    for (let[index, dist] : distances_sorted) {
+      if (dist == farthest_dist) {
+        score[index] += 1;
+      } else {
+        break;
+      }
+    }
+  }
+  return std::ranges::max(score);
+}
+
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT(1120, solve_case<1000>("day14.example"));
-  AOC_EXPECT_RESULT(2660, solve_case<2503>("day14.input"));
-  // std::println("Part 2");
-  // AOC_EXPECT_RESULT(4, solve_case<true>("day14.example"));
-  // AOC_EXPECT_RESULT(96852, solve_case<true>("day14.input"));
+  AOC_EXPECT_RESULT(1120, solve_case1<1000>("day14.example"));
+  AOC_EXPECT_RESULT(2660, solve_case1<2503>("day14.input"));
+  std::println("Part 2");
+  AOC_EXPECT_RESULT(689, solve_case2<1000>("day14.example"));
+  AOC_EXPECT_RESULT(1256, solve_case2<2503>("day14.input"));
   AOC_RETURN_CHECK_RESULT();
 }
--- rust/2015/src/day14.rs
+++ rust/2015/src/day14.rs
@@ -19,7 +19,7 @@ fn parse(filename: &str) -> Vec<Reindeer> {
         .collect()
 }
 
-fn solve_case<const SECONDS: u32>(filename: &str) -> u32 {
+fn solve_case1<const SECONDS: u32>(filename: &str) -> u32 {
     parse(filename)
         .iter()
         .map(|reindeer| {
@@ -34,11 +34,38 @@ fn solve_case<const SECONDS: u32>(filename: &str) -> u32 {
         .unwrap()
 }
 
+fn solve_case2<const SECONDS: u32>(filename: &str) -> u32 {
+    let flock = parse(filename);
+    let mut distances = [0u32; 9];
+    let mut score = [0u32; 9];
+    debug_assert!(flock.len() <= score.len(), "Too many reindeer");
+    for second in 0..SECONDS {
+        // Move each reindeer
+        for (index, reindeer) in flock.iter().enumerate() {
+            let cycle_time = reindeer.fly + reindeer.rest;
+            let flying = (second % cycle_time) < reindeer.fly;
+            distances[index] += (flying as u32) * reindeer.kms;
+        }
+        // Award the farthest reindeer
+        let mut distances_sorted = distances.iter().enumerate().collect::<Vec<_>>();
+        distances_sorted.sort_by_key(|(_, dist)| **dist);
+        let farthest_dist = distances_sorted.last().unwrap().1;
+        for (index, dist) in distances_sorted.iter().rev() {
+            if (*dist == farthest_dist) {
+                score[*index] += 1;
+            } else {
+                break;
+            }
+        }
+    }
+    return *score.iter().max().unwrap();
+}
+
 fn main() {
     println!("Part 1");
-    assert_eq!(1120, solve_case::<1000>("day14.example"));
-    assert_eq!(2660, solve_case::<2503>("day14.input"));
-    // println!("Part 2");
-    // assert_eq!(4, solve_case2("day14.example"));
-    // assert_eq!(96852, solve_case2("day14.input"));
+    assert_eq!(1120, solve_case1::<1000>("day14.example"));
+    assert_eq!(2660, solve_case1::<2503>("day14.input"));
+    println!("Part 2");
+    assert_eq!(689, solve_case2::<1000>("day14.example"));
+    assert_eq!(1256, solve_case2::<2503>("day14.input"));
 }
