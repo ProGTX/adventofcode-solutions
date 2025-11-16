4328a060d75e8f857754b8f242d5e024a1c38480
Peter Žužek <peterzuzek@gmail.com>
2025-11-16 20:31:02 +0100

    2015 day 16 part 2

--- 2015/day16.cpp
+++ 2015/day16.cpp
@@ -9,6 +9,7 @@
 #include <print>
 #include <ranges>
 
+template <bool RANGES>
 fn solve_case(String const& filename) -> u32 {
   let generous_sue = aoc::flat_map<str, u32>{
       {"children", 3}, {"cats", 7},     {"samoyeds", 2}, {"pomeranians", 3},
@@ -22,7 +23,19 @@ fn solve_case(String const& filename) -> u32 {
     let split_info = aoc::split_to_array<3>(all_info[1], ", ");
     let matches = std::ranges::all_of(split_info, [&](str info) {
       let[key, value] = aoc::split_once(info, ": ");
-      return generous_sue.at(key) == aoc::to_number<u32>(value);
+      let required = generous_sue.at(key);
+      let actual = aoc::to_number<u32>(value);
+      if constexpr (!RANGES) {
+        return actual == required;
+      } else {
+        if ((key == "cats") || (key == "trees")) {
+          return actual > required;
+        } else if ((key == "pomeranians") || (key == "goldfish")) {
+          return actual < required;
+        } else {
+          return actual == required;
+        }
+      }
     });
     if (matches) {
       return static_cast<u32>(index + 1);
@@ -33,8 +46,8 @@ fn solve_case(String const& filename) -> u32 {
 
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT(373, solve_case("day16.input"));
-  // std::println("Part 2");
-  // AOC_EXPECT_RESULT(1766400, solve_case<true>("day16.input"));
+  AOC_EXPECT_RESULT(373, solve_case<false>("day16.input"));
+  std::println("Part 2");
+  AOC_EXPECT_RESULT(260, solve_case<true>("day16.input"));
   AOC_RETURN_CHECK_RESULT();
 }
--- rust/2015/src/day16.rs
+++ rust/2015/src/day16.rs
@@ -1,18 +1,18 @@
 use std::collections::HashMap;
 
-fn solve_case(filename: &str) -> u32 {
-    let mut generous_sue = HashMap::new();
-    generous_sue.insert("children", 3);
-    generous_sue.insert("cats", 7);
-    generous_sue.insert("samoyeds", 2);
-    generous_sue.insert("pomeranians", 3);
-    generous_sue.insert("akitas", 0);
-    generous_sue.insert("vizslas", 0);
-    generous_sue.insert("goldfish", 5);
-    generous_sue.insert("trees", 3);
-    generous_sue.insert("cars", 2);
-    generous_sue.insert("perfumes", 1);
-    let generous_sue = generous_sue;
+fn solve_case<const RANGES: bool>(filename: &str) -> u32 {
+    let generous_sue = HashMap::from([
+        ("children", 3),
+        ("cats", 7),
+        ("samoyeds", 2),
+        ("pomeranians", 3),
+        ("akitas", 0),
+        ("vizslas", 0),
+        ("goldfish", 5),
+        ("trees", 3),
+        ("cars", 2),
+        ("perfumes", 1),
+    ]);
 
     for (index, line) in std::fs::read_to_string(filename)
         .unwrap()
@@ -23,7 +23,17 @@ fn solve_case(filename: &str) -> u32 {
         let split_info = info.split(", ");
         let matches = split_info.into_iter().all(|info| {
             let (key, value) = info.split_once(": ").unwrap();
-            return *generous_sue.get(key).unwrap() == value.parse::<u32>().unwrap();
+            let required = *generous_sue.get(key).unwrap();
+            let actual = value.parse::<u32>().unwrap();
+            if (!RANGES) {
+                return actual == required;
+            } else {
+                return match key {
+                    "cats" | "trees" => actual > required,
+                    "pomeranians" | "goldfish" => actual < required,
+                    _ => actual == required,
+                };
+            }
         });
         if (matches) {
             return (index + 1) as u32;
@@ -34,7 +44,7 @@ fn solve_case(filename: &str) -> u32 {
 
 fn main() {
     println!("Part 1");
-    assert_eq!(373, solve_case("day16.input"));
-    // println!("Part 2");
-    // assert_eq!(1766400, solve_case::<true>("day16.input"));
+    assert_eq!(373, solve_case::<false>("day16.input"));
+    println!("Part 2");
+    assert_eq!(260, solve_case::<true>("day16.input"));
 }
