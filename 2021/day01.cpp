e1a08565e8a1a35939953fdbe1d92a05d8947a91
Peter Žužek <peterzuzek@gmail.com>
2025-11-20 10:38:57 +0100

    2021 day 1 C++ modernized

--- 2021/day01.cpp
+++ 2021/day01.cpp
@@ -1,56 +1,42 @@
 // https://adventofcode.com/2021/day/1
 
 #include "../common/common.h"
+#include "../common/rust.h"
 
 #include <algorithm>
 #include <iostream>
-#include <iterator>
-#include <numeric>
-#include <ostream>
+#include <print>
 #include <ranges>
-#include <string>
-#include <string_view>
-#include <vector>
+#include <span>
 
-int solve_case(const std::string& filename, int window_width) {
-  auto measurements = aoc::views::read_numbers<int>(filename) |
-                      aoc::ranges::to<std::vector<int>>();
-  const auto displacement = window_width - 1;
+fn parse(String const& filename) -> Vec<u32> {
+  return aoc::views::read_numbers<u32>(filename) | aoc::ranges::to<Vec<u32>>();
+}
 
-  const auto it = std::begin(measurements);
-  for (int i = displacement; i < measurements.size(); ++i) {
-    auto window_start = it + i - displacement;
-    auto window_end = window_start + window_width;
-    auto sum = std::accumulate(window_start, window_end, 0);
-    // Inline modification
-    *window_start = sum;
-  }
+fn solve_case1(std::span<const u32> depths) -> usize {
+  return std::ranges::count_if(
+      depths | std::views::slide(2),
+      [](auto&& window) { return window[1] > window[0]; });
+}
 
-  namespace views = std::ranges::views;
-
-  std::vector<int> diffs;
-  int previousMeasure = 0;
-  const auto diffFunc = [&](int currentMeasure) {
-    diffs.push_back(currentMeasure - previousMeasure);
-    previousMeasure = currentMeasure;
-  };
-  std::ranges::for_each(
-      measurements | views::take(measurements.size() - displacement), diffFunc);
-
-  auto relevantDiffs = diffs | views::drop(1);
-  auto count =
-      std::ranges::count_if(relevantDiffs, [](int diff) { return diff > 0; });
-
-  std::cout << filename << " -> " << count << std::endl;
-  return count;
+fn solve_case2(std::span<const u32> depths) -> usize {
+  return solve_case1(depths |
+                     std::views::slide(3) |
+                     std::views::transform([](auto&& window) {
+                       return aoc::ranges::accumulate(window, 0u);
+                     }) |
+                     aoc::ranges::to<Vec<u32>>());
 }
 
 int main() {
-  std::cout << "Part 1" << std::endl;
-  AOC_EXPECT_RESULT(7, solve_case("day01.example", 1));
-  AOC_EXPECT_RESULT(1228, solve_case("day01.input", 1));
-  std::cout << "Part 2" << std::endl;
-  AOC_EXPECT_RESULT(5, solve_case("day01.example", 3));
-  AOC_EXPECT_RESULT(1257, solve_case("day01.input", 3));
+  std::println("Part 1");
+  let example = parse("day01.example");
+  AOC_EXPECT_RESULT(7, solve_case1(example));
+  let input = parse("day01.input");
+  AOC_EXPECT_RESULT(1228, solve_case1(input));
+
+  std::println("Part 2");
+  AOC_EXPECT_RESULT(5, solve_case2(example));
+  AOC_EXPECT_RESULT(1257, solve_case2(input));
   AOC_RETURN_CHECK_RESULT();
 }
