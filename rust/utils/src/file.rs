75557e181c64367747058cdf4b22190918efde63
Peter Žužek <peterzuzek@gmail.com>
2025-11-21 14:54:56 +0100

    aoc::file::read_string in Rust

--- rust/utils/src/file.rs
+++ rust/utils/src/file.rs
@@ -4,6 +4,10 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::str::FromStr;
 
+pub fn read_string(filename: &str) -> String {
+    std::fs::read_to_string(filename).unwrap()
+}
+
 // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
 pub fn read_lines<P>(filename: P) -> io::Lines<io::BufReader<File>>
 where
