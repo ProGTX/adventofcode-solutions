0ac9c67fbf6dd3067fc80292250b8ae0b71d83cf
Peter Žužek <peterzuzek@gmail.com>
2025-11-21 08:58:41 +0100

    2015 day 21 part 1 C++

---
+++ 2015/day21.cpp
@@ -0,0 +1,153 @@
+// https://adventofcode.com/2015/day/21
+
+#include "../common/common.h"
+#include "../common/rust.h"
+
+#include <algorithm>
+#include <iostream>
+#include <print>
+
+struct Entity {
+  u32 value;
+  u32 damage;
+  u32 armor;
+
+  // Returns true if victim was defeated
+  fn attack(Entity& victim) const -> bool {
+    let actual_damage = [&] -> u32 {
+      if (damage <= victim.armor) {
+        return 1;
+      } else {
+        return damage - victim.armor;
+      };
+    }();
+    if (actual_damage >= victim.value) {
+      victim.value = 0;
+      return true;
+    } else {
+      victim.value -= actual_damage;
+      return false;
+    };
+  }
+};
+
+// Returns true if player won
+fn play_game(Entity& player, Entity& boss) -> bool {
+  loop {
+    if (player.attack(boss)) {
+      return true;
+    }
+    if (boss.attack(player)) {
+      return false;
+    }
+  }
+}
+
+fn parse(String const& filename) -> Entity {
+  auto lines_view = aoc::views::read_lines(filename);
+  auto line_it = std::ranges::begin(lines_view);
+  let parse_line = [&] {
+    let[_, value_str] = aoc::split_once(*line_it, ": ");
+    let value = aoc::to_number<u32>(value_str);
+    ++line_it;
+    return value;
+  };
+  return Entity{
+      .value = parse_line(),
+      .damage = parse_line(),
+      .armor = parse_line(),
+  };
+}
+
+constexpr const auto shop_weapons = std::array{
+    Entity{8, 4, 0},  //
+    Entity{10, 5, 0}, //
+    Entity{25, 6, 0}, //
+    Entity{40, 7, 0}, //
+    Entity{74, 8, 0}, //
+};
+constexpr const auto shop_armor = std::array{
+    Entity{13, 0, 1},  //
+    Entity{31, 0, 2},  //
+    Entity{53, 0, 3},  //
+    Entity{75, 0, 4},  //
+    Entity{102, 0, 5}, //
+};
+constexpr const auto shop_rings = std::array{
+    Entity{20, 0, 1},  //
+    Entity{25, 1, 0},  //
+    Entity{40, 0, 2},  //
+    Entity{50, 2, 0},  //
+    Entity{80, 0, 3},  //
+    Entity{100, 3, 0}, //
+};
+
+template <class T>
+struct inspect_t;
+
+template <u32 START_HP>
+fn solve_case1(Entity const& input) -> u32 {
+  auto best_cost = aoc::as_consteval(shop_weapons[4].value +
+                                     shop_armor[4].value +
+                                     shop_rings[4].value +
+                                     shop_rings[5].value);
+  for (let weapon : shop_weapons) {
+    aoc::gen_combinations(
+        shop_armor,
+        aoc::combinations_args<u32>{
+            .single_min = 0,
+            .single_max = 1,
+            .all_min = 0,
+            .all_max = 1,
+        },
+        [&](auto&& armor_combo) {
+          let armor = aoc::binary_select_from_combination<Option<Entity>>(
+                          shop_armor, armor_combo)
+                          .value_or(Entity{});
+          aoc::gen_combinations(
+              shop_rings,
+              aoc::combinations_args<u32>{
+                  .single_min = 0,
+                  .single_max = 1,
+                  .all_min = 0,
+                  .all_max = 2,
+              },
+              [&](auto&& rings_combo) {
+                let ring_pair =
+                    aoc::binary_select_from_combination<std::array<Entity, 2>>(
+                        shop_rings, rings_combo);
+                auto player = Entity{
+                    .value = START_HP,
+                    .damage = weapon.damage +
+                              ring_pair[0].damage +
+                              ring_pair[1].damage,
+                    .armor =
+                        armor.armor + ring_pair[0].armor + ring_pair[1].armor,
+                };
+                auto boss = input;
+                if (play_game(player, boss)) {
+                  best_cost = std::min(weapon.value +
+                                           armor.value +
+                                           ring_pair[0].value +
+                                           ring_pair[1].value,
+                                       best_cost);
+                }
+              });
+        });
+  }
+  return best_cost;
+}
+
+int main() {
+  std::println("Part 1");
+  let example = parse("day21.example");
+  AOC_EXPECT_RESULT(65, solve_case1<8>(example));
+  let input = parse("day21.input");
+  AOC_EXPECT_RESULT(111, solve_case1<100>(input));
+
+  // std::println("Part 2");
+  // AOC_EXPECT_RESULT(3, solve_case2(example));
+  // AOC_EXPECT_RESULT(1257, solve_case2(input));
+
+  AOC_RETURN_CHECK_RESULT();
+}
