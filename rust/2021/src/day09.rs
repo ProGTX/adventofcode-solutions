6ce7b0215568d994e641b7b02c4fc143981ea075
Peter Žužek <peterzuzek@gmail.com>
2025-12-05 15:56:59 +0100

    [Rust] 2021 day 9 part 2

--- rust/2021/src/day09.rs
+++ rust/2021/src/day09.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::collections::HashSet;
+
 type Heightmap = aoc::grid::Grid<u32>;
 type LowPoints = Vec<(usize, u32)>;
 
@@ -27,6 +30,39 @@ fn solve_case1(heightmap: &Heightmap) -> u32 {
         .sum()
 }
 
+fn solve_case2(heightmap: &Heightmap) -> u32 {
+    get_low_points(heightmap)
+        .iter()
+        .map(|(origin, _)| {
+            let mut visited = HashSet::new();
+            let mut unvisited = Vec::new();
+            unvisited.push(*origin);
+            while (!unvisited.is_empty()) {
+                let index = unvisited.pop().unwrap();
+                if (!visited.insert(index)) {
+                    continue;
+                }
+                let pos = heightmap.position(index);
+                let current = heightmap.get(pos.y, pos.x);
+                for neighbor in heightmap.basic_neighbor_positions(pos) {
+                    let neighbor_index = heightmap.linear_index(neighbor.y, neighbor.x);
+                    let neighbor = heightmap.get(neighbor.y, neighbor.x);
+                    if ((*neighbor != 9)
+                        && (neighbor > current)
+                        && (!visited.contains(&neighbor_index)))
+                    {
+                        unvisited.push(neighbor_index);
+                    }
+                }
+            }
+            visited.len() as u32
+        })
+        .sorted_unstable()
+        .rev()
+        .take(3)
+        .product()
+}
+
 fn main() {
     println!("Part 1");
     let example = parse("day09.example");
@@ -34,7 +70,7 @@ fn main() {
     let input = parse("day09.input");
     assert_eq!(580, solve_case1(&input));
 
-    //println!("Part 2");
-    //assert_eq!(1134, solve_case2(&example));
-    //assert_eq!(856716, solve_case2(&input));
+    println!("Part 2");
+    assert_eq!(1134, solve_case2(&example));
+    assert_eq!(856716, solve_case2(&input));
 }
