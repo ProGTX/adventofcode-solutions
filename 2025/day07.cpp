40f4c3d891380bb1d0959aa950d377b04aad0f22
Peter Žužek <peterzuzek@gmail.com>
2025-12-09 19:32:58 +0100

    2025 day 7 part 2
    Uses a new method of scanning rows. This also simplifies part 1.
    
    Added new example while trying out a breadth-first approach,
    which was abandoned, but it doesn't hurt to have an extra example.

--- 2025/day07.cpp
+++ 2025/day07.cpp
@@ -7,61 +7,64 @@
 #include <iostream>
 #include <print>
 #include <ranges>
-#include <set>
 
 using Manifolds = aoc::char_grid<>;
 using Input = std::pair<Manifolds, point>;
-constexpr let wall = '#';
 constexpr let empty = '.';
 constexpr let splitter = '^';
 
 fn parse(String const& filename) -> Input {
   auto [manifolds, config] = aoc::read_char_grid(
-      filename, {.padding = wall, .start_char = 'S', .end_char = {}});
+      filename, {.padding = {}, .start_char = 'S', .end_char = {}});
   return {std::move(manifolds), *config.start_pos};
 }
 
-fn solve_case1(Input const& input) -> u32 {
+template <bool quantum>
+fn solve_case(Input const& input) -> u64 {
   let & [ manifolds, start_pos ] = input;
-  auto beams = std::set<point>{};
-  beams.insert(start_pos);
-  auto num_splits = 0u;
-  while (!beams.empty()) {
-    auto next_beams = std::set<point>{};
-    for (auto beam_pos : beams) {
-      beam_pos += aoc::get_diff(aoc::south);
-      let value = manifolds.at(beam_pos.y, beam_pos.x);
+  auto num_splits = 0;
+  auto row_timelines = Vec(manifolds.num_columns(), u64{});
+  row_timelines[start_pos.x] = 1;
+  for (let row :
+       Range{static_cast<usize>(start_pos.y + 1), manifolds.num_rows()}) {
+    auto next_timelines = Vec(manifolds.num_columns(), u64{});
+    for (let[column, timelines] : row_timelines | std::views::enumerate) {
+      let value = manifolds.at(row, column);
       switch (value) {
         case empty:
-          next_beams.insert(beam_pos);
+          next_timelines[column] += timelines;
           break;
-        case splitter:
-          num_splits += 1;
-          next_beams.insert(beam_pos + aoc::get_diff(aoc::west));
-          next_beams.insert(beam_pos + aoc::get_diff(aoc::east));
-          break;
-        case wall:
-          // Do nothing
-          break;
+        case splitter: {
+          num_splits += static_cast<u64>(timelines > 0);
+          next_timelines[column - 1] += timelines;
+          next_timelines[column + 1] += timelines;
+        } break;
         default:
           AOC_UNREACHABLE("Invalid value in the manifold");
-      }
+      };
     }
-    beams = std::move(next_beams);
+    row_timelines = std::move(next_timelines);
   }
-  return num_splits;
+  if constexpr (!quantum) {
+    return num_splits;
+  } else {
+    return aoc::ranges::accumulate(row_timelines, u64{});
+  }
 }
 
 int main() {
   std::println("Part 1");
   let example = parse("day07.example");
-  AOC_EXPECT_RESULT(21, solve_case1(example));
+  AOC_EXPECT_RESULT(21, solve_case<false>(example));
+  let example2 = parse("day07.example2");
+  AOC_EXPECT_RESULT(16, solve_case<false>(example2));
   let input = parse("day07.input");
-  AOC_EXPECT_RESULT(1560, solve_case1(input));
+  AOC_EXPECT_RESULT(1560, solve_case<false>(input));
 
-  // std::println("Part 2");
-  // AOC_EXPECT_RESULT(40, solve_case2(example));
-  // AOC_EXPECT_RESULT(9876636978528, solve_case2(input));
+  std::println("Part 2");
+  AOC_EXPECT_RESULT(40, solve_case<true>(example));
+  AOC_EXPECT_RESULT(26, solve_case<true>(example2));
+  AOC_EXPECT_RESULT(25592971184998, solve_case<true>(input));
 
   AOC_RETURN_CHECK_RESULT();
 }
--- rust/2025/src/day07.rs
+++ rust/2025/src/day07.rs
@@ -1,8 +1,5 @@
-use std::collections::HashSet;
-
 type Manifolds = aoc::grid::Grid<char>;
 type Upos = aoc::grid::Upos;
-const WALL: char = '#';
 const EMPTY: char = '.';
 const SPLITTER: char = '^';
 
@@ -10,7 +7,7 @@ fn parse(filename: &str) -> (Manifolds, Upos) {
     let (manifolds, config) = Manifolds::from_file_config(
         filename,
         aoc::grid::ConfigInput {
-            padding: Some(WALL),
+            padding: None,
             start_char: Some('S'),
             end_char: None,
         },
@@ -18,43 +15,46 @@ fn parse(filename: &str) -> (Manifolds, Upos) {
     (manifolds, config.start_pos.unwrap())
 }
 
-fn solve_case1((manifolds, start_pos): &(Manifolds, Upos)) -> usize {
-    let mut beams = HashSet::new();
-    beams.insert(*start_pos);
+fn solve_case<const QUANTUM: bool>((manifolds, start_pos): &(Manifolds, Upos)) -> u64 {
     let mut num_splits = 0;
-    while (!beams.is_empty()) {
-        let mut next_beams = HashSet::new();
-        for beam_pos_orig in beams {
-            let beam_pos = beam_pos_orig + Upos::new(0, 1);
-            let value = manifolds.get(beam_pos.y, beam_pos.x);
-            match *value {
+    let mut row_timelines = vec![0; manifolds.num_columns];
+    row_timelines[start_pos.x] = 1;
+    for row in (start_pos.y + 1)..(manifolds.num_rows) {
+        let mut next_timelines = vec![0; manifolds.num_columns];
+        for (column, &timelines) in row_timelines.iter().enumerate() {
+            let value = *manifolds.get(row, column);
+            match value {
                 EMPTY => {
-                    next_beams.insert(beam_pos);
+                    next_timelines[column] += timelines;
                 }
                 SPLITTER => {
-                    num_splits += 1;
-                    next_beams.insert(Upos::new(beam_pos.x - 1, beam_pos.y));
-                    next_beams.insert(beam_pos + Upos::new(1, 0));
-                }
-                WALL => {
-                    // Do nothing
+                    num_splits += (timelines > 0) as u64;
+                    next_timelines[column - 1] += timelines;
+                    next_timelines[column + 1] += timelines;
                 }
                 _ => unreachable!("Invalid value in the manifold"),
             };
         }
-        beams = next_beams;
+        row_timelines = next_timelines;
     }
-    return num_splits;
+    return if (!QUANTUM) {
+        num_splits
+    } else {
+        row_timelines.iter().sum()
+    };
 }
 
 fn main() {
     println!("Part 1");
     let example = parse("day07.example");
-    assert_eq!(21, solve_case1(&example));
+    assert_eq!(21, solve_case::<false>(&example));
+    let example2 = parse("day07.example2");
+    assert_eq!(16, solve_case::<false>(&example2));
     let input = parse("day07.input");
-    assert_eq!(1560, solve_case1(&input));
+    assert_eq!(1560, solve_case::<false>(&input));
 
-    // println!("Part 2");
-    // assert_eq!(40, solve_case2(&example));
-    // assert_eq!(9876636978528, solve_case2(&input));
+    println!("Part 2");
+    assert_eq!(40, solve_case::<true>(&example));
+    assert_eq!(26, solve_case::<true>(&example2));
+    assert_eq!(25592971184998, solve_case::<true>(&input));
 }
