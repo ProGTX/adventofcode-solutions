05ed2420e2dce9548339821df0023e6ecd8d6519
Peter Žužek <peterzuzek@gmail.com>
2026-04-29 17:10:08 +0200

    Scaffolding for rest of 2021

---
+++ 2021/day20.cpp
@@ -0,0 +1,39 @@
+// https://adventofcode.com/2021/day/20
+
+#include "../common/common.h"
+#include "../common/rust.h"
+
+#include <iostream>
+#include <print>
+#include <span>
+#include <string>
+#include <vector>
+
+auto parse(String const& filename) -> Vec<std::string> {
+  return aoc::views::read_lines(filename) | aoc::ranges::to<Vec<std::string>>();
+}
+
+fn solve_case1(std::span<const std::string> data) -> uint64_t {
+  // TODO: Implement Part 1
+  return 0;
+}
+
+fn solve_case2(std::span<const std::string> data) -> uint64_t {
+  // TODO: Implement Part 2
+  return 0;
+}
+
+int main() {
+  std::println("Part 1");
+  const auto example = parse("day20.example");
+  AOC_EXPECT_RESULT(0, solve_case1(example));
+
+  const auto input = parse("day20.input");
+  // AOC_EXPECT_RESULT(XXX, solve_case1(input));
+
+  std::println("Part 2");
+  // AOC_EXPECT_RESULT(XXX, solve_case2(example));
+  // AOC_EXPECT_RESULT(XXX, solve_case2(input));
+
+  AOC_RETURN_CHECK_RESULT();
+}
---
+++ rust/2021/src/day19.rs
@@ -0,0 +1,26 @@
+fn parse(filename: &str) -> Vec<String> {
+    aoc::file::read_lines(filename)
+}
+
+fn solve_case1(data: &[String]) -> u64 {
+    // TODO: Implement Part 1
+    0
+}
+
+fn solve_case2(data: &[String]) -> u64 {
+    // TODO: Implement Part 2
+    0
+}
+
+fn main() {
+    println!("Part 1");
+    let example = parse("day19.example");
+    assert_eq!(0, solve_case1(&example));
+
+    let input = parse("day19.input");
+    // assert_eq!(XXX, solve_case1(&input));
+
+    println!("Part 2");
+    // assert_eq!(XXX, solve_case2(&example));
+    // assert_eq!(XXX, solve_case2(&input));
+}
