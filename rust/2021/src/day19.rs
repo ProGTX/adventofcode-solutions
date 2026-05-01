093f086f8e79d8b93f0f4fb03a501ffeac786fcb
Peter Žužek <peterzuzek@gmail.com>
2026-05-01 11:00:18 +0200

    2021 day 19 start

--- rust/2021/src/day19.rs
+++ rust/2021/src/day19.rs
@@ -1,4 +1,5 @@
 use aoc::nd_point::NDPoint;
+use std::collections::{HashMap, HashSet};
 
 type Point3D = NDPoint<i32, 3>;
 type Scanner = Vec<Point3D>;
@@ -26,14 +27,49 @@ fn parse(filename: &str) -> Vec<Scanner> {
     scanners
 }
 
-#[allow(dead_code)]
-fn solve_case1(_data: &[Scanner]) -> usize {
-    // TODO: Implement Part 1
-    0
+fn rotations(scanner: &Scanner) -> impl Iterator<Item = Scanner> + '_ {
+    // 6 axis permutations times 4 sign combos where det(rotation) = +1
+    let transforms: [fn(Point3D) -> Point3D; 24] = [
+        // permutation (x,y,z), det=+1 -> signs product must be +1
+        |Point3D { data: [x, y, z] }| Point3D { data: [x, y, z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [x, y, -z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [x, -y, z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [x, -y, -z] },
+        // permutation (x,z,y), det=-1 -> signs product must be -1
+        |Point3D { data: [x, y, z] }| Point3D { data: [-x, z, y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-x, -z, y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-x, z, -y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-x, -z, -y] },
+        // permutation (y,x,z), det=-1 -> signs product must be -1
+        |Point3D { data: [x, y, z] }| Point3D { data: [-y, x, z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [y, -x, z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [y, x, -z] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-y, -x, -z] },
+        // permutation (y,z,x), det=+1 -> signs product must be +1
+        |Point3D { data: [x, y, z] }| Point3D { data: [y, z, x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [y, -z, x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-y, z, x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-y, -z, x] },
+        // permutation (z,x,y), det=+1 -> signs product must be +1
+        |Point3D { data: [x, y, z] }| Point3D { data: [z, x, y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [z, -x, y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-z, x, y] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-z, -x, y] },
+        // permutation (z,y,x), det=-1 -> signs product must be -1
+        |Point3D { data: [x, y, z] }| Point3D { data: [-z, y, x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [z, y, -x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [z, -y, -x] },
+        |Point3D { data: [x, y, z] }| Point3D { data: [-z, -y, x] },
+    ];
+    transforms
+        .into_iter()
+        .map(|f| scanner.iter().map(|&p| f(p)).collect())
 }
 
-#[allow(dead_code)]
-fn solve_case2(_data: &[Scanner]) -> usize {
+fn solve_case1(scanners: &[Scanner]) -> usize {
+}
+
+fn solve_case2(scanners: &[Scanner]) -> usize {
     // TODO: Implement Part 2
     0
 }
@@ -42,10 +78,10 @@ fn main() {
     println!("UNSOLVED");
 
     println!("Part 1");
-    let _example = parse("day19.example");
-    // aoc::expect_result!(79, solve_case1(&_example));
-    let _input = parse("day19.input");
-    // aoc::expect_result!(XXX, solve_case1(&input));
+    let example = parse("day19.example");
+    assert_eq!(79, solve_case1(&example));
+    let input = parse("day19.input");
+    // assert_eq!(XXX, solve_case1(&input));
 
     println!("Part 2");
     // aoc::expect_result!(XXX, solve_case2(&example));
