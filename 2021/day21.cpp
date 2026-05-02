f74cc5512c09dbee91906839ddfbf0e1525995dd
Peter Žužek <peterzuzek@gmail.com>
2026-05-02 18:34:59 +0200

    [C++] 2021 day 21 part 1

--- 2021/day21.cpp
+++ 2021/day21.cpp
@@ -5,31 +5,65 @@
 
 #include <iostream>
 #include <print>
-#include <span>
 #include <string>
 #include <vector>
 
-auto parse(String const& filename) -> Vec<std::string> {
-  return aoc::views::read_lines(filename) | aoc::ranges::to<Vec<std::string>>();
+using Input = std::pair<u32, u32>;
+
+fn parse(String const& filename) -> Input {
+  auto lines = aoc::views::read_lines(filename);
+  auto current_line = std::begin(lines);
+  let p1 = aoc::to_number<u32>(
+      aoc::trim(*current_line).substr(sizeof("Player 1 starting position:")));
+  ++current_line;
+  let p2 = aoc::to_number<u32>(
+      aoc::trim(*current_line).substr(sizeof("Player 2 starting position:")));
+  return {p1, p2};
 }
 
-fn solve_case1(std::span<const std::string> data) -> uint64_t {
-  // TODO: Implement Part 1
-  return 0;
+struct DeterministicDice {
+  u32 current{0};
+  fn roll() -> u32 {
+    current = current % 100 + 1;
+    return current;
+  }
+};
+
+fn solve_case1(Input positions) -> u32 {
+  auto [p1, p2] = positions;
+  auto score = std::pair<u32, u32>{0, 0};
+  auto rolls = u32{};
+  auto dice = DeterministicDice{};
+  auto roll_dice = [&](u32 pos) -> u32 {
+    let rolled = dice.roll() + dice.roll() + dice.roll();
+    rolls += 3;
+    return (pos + rolled - 1) % 10 + 1;
+  };
+  loop {
+    p1 = roll_dice(p1);
+    score.first += p1;
+    if (score.first >= 1000) {
+      return score.second * rolls;
+    }
+    p2 = roll_dice(p2);
+    score.second += p2;
+    if (score.second >= 1000) {
+      return score.first * rolls;
+    }
+  }
 }
 
-fn solve_case2(std::span<const std::string> data) -> uint64_t {
+fn solve_case2(Input positions) -> u64 {
   // TODO: Implement Part 2
   return 0;
 }
 
 int main() {
   std::println("Part 1");
-  const auto example = parse("day21.example");
-  AOC_EXPECT_RESULT(0, solve_case1(example));
-
-  const auto input = parse("day21.input");
-  // AOC_EXPECT_RESULT(XXX, solve_case1(input));
+  let example = parse("day21.example");
+  AOC_EXPECT_RESULT(739785, solve_case1(example));
+  let input = parse("day21.input");
+  AOC_EXPECT_RESULT(805932, solve_case1(input));
 
   std::println("Part 2");
   // AOC_EXPECT_RESULT(XXX, solve_case2(example));
