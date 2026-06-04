660bf0fd998c63d355241f97d375c208aabe9884
Peter Žužek <peterzuzek@gmail.com>
2026-06-04 09:57:01 +0200

    Initial try at compressing SearchNode

--- rust/2022/src/day19.rs
+++ rust/2022/src/day19.rs
@@ -48,50 +48,127 @@ fn try_build_robot(
     Some(new_resources)
 }
 
-fn collect_resources(robots: &Robots, resources: &Resources) -> Resources {
-    let mut new_resources = resources.clone();
-    for robot_id in (0..NUM_ROBOTS) {
-        new_resources[robot_id] += robots[robot_id];
-    }
-    new_resources
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+struct SearchNode {
+    bits: u64,
 }
 
-#[derive(Debug, Eq, Hash, PartialEq)]
-struct SearchNode {
-    robots: Robots,
-    resources: Resources,
-    time_left: u8,
+impl SearchNode {
+    const A_BITS: u64 = 0x3f; // 6 bits
+    const B_BITS: u64 = 0x7f; // 7 bits; 
+
+    const A_SHIFT: [u32; 4] = [0, 6, 12, 18];
+    const B_SHIFT: [u32; 4] = [24, 31, 38, 45];
+    const TIME_SHIFT: u32 = 52;
+
+    fn new(robots: Robots, resources: Resources, time_left: u8) -> Self {
+        let mut node = Self { bits: 0 };
+
+        for i in 0..4 {
+            node.set_robot(i, robots[i]);
+            node.set_resource(i, resources[i]);
+        }
+
+        node.set_time_left(time_left);
+        node
+    }
+
+    fn robots(&self, idx: usize) -> u8 {
+        assert!(idx < 4);
+        ((self.bits >> Self::A_SHIFT[idx]) & Self::A_BITS) as u8
+    }
+
+    fn set_robot(&mut self, idx: usize, value: u8) {
+        assert!(idx < 4);
+        assert!(value < 64);
+
+        let shift = Self::A_SHIFT[idx];
+        let mask = Self::A_BITS << shift;
+
+        self.bits &= !mask;
+        self.bits |= (value as u64) << shift;
+    }
+
+    fn resources(&self, idx: usize) -> u8 {
+        assert!(idx < 4);
+        ((self.bits >> Self::B_SHIFT[idx]) & Self::B_BITS) as u8
+    }
+
+    fn set_resource(&mut self, idx: usize, value: u8) {
+        assert!(idx < 4);
+        assert!(value < 128);
+
+        let shift = Self::B_SHIFT[idx];
+        let mask = Self::B_BITS << shift;
+
+        self.bits &= !mask;
+        self.bits |= (value as u64) << shift;
+    }
+
+    fn time_left(&self) -> u8 {
+        (self.bits >> Self::TIME_SHIFT) as u8
+    }
+
+    fn set_time_left(&mut self, value: u8) {
+        let mask = 0xffu64 << Self::TIME_SHIFT;
+        self.bits &= !mask;
+        self.bits |= (value as u64) << Self::TIME_SHIFT;
+    }
+
+    fn robots_array(&self) -> Robots {
+        [
+            self.robots(0),
+            self.robots(1),
+            self.robots(2),
+            self.robots(3),
+        ]
+    }
+
+    fn resources_array(&self) -> Resources {
+        [
+            self.resources(0),
+            self.resources(1),
+            self.resources(2),
+            self.resources(3),
+        ]
+    }
+
+    fn collect_resources(&self) -> Resources {
+        let mut new_resources = self.resources_array();
+        for robot_id in (0..NUM_ROBOTS) {
+            new_resources[robot_id] += self.robots(robot_id);
+        }
+        new_resources
+    }
 }
 
 type Cache = HashMap<SearchNode, u16>;
 
 fn max_open_geodes(cache: &mut Cache, blueprint: &Blueprint, search_node: SearchNode) -> u16 {
-    if (search_node.time_left == 1) {
+    let time_left = search_node.time_left();
+    if (time_left == 1) {
         // Doesn't make sense to build robots in last minute
-        let new_resources = collect_resources(&search_node.robots, &search_node.resources);
+        let new_resources = search_node.collect_resources();
         return new_resources[GEODE_ID] as u16;
     }
-    debug_assert_ne!(0, search_node.time_left, "Time should never reach 0");
+    debug_assert_ne!(0, time_left, "Time should never reach 0");
     if let Some(&cached_result) = cache.get(&search_node) {
         return cached_result;
     }
     // Try building each type of robot with the resources available
+    let robots = search_node.robots_array();
+    let resources = search_node.resources_array();
     let num_geodes = (0..NUM_ROBOTS)
         .filter_map(|robot_id| {
-            if let Some(new_resources) =
-                try_build_robot(blueprint, &search_node.resources, robot_id)
-            {
-                let new_resources = collect_resources(&search_node.robots, &new_resources);
-                let mut new_robots = search_node.robots.clone();
+            if let Some(new_resources) = try_build_robot(blueprint, &resources, robot_id) {
+                let temp_node = SearchNode::new(robots.clone(), new_resources, time_left);
+                let new_resources = temp_node.collect_resources();
+                let mut new_robots = robots.clone();
                 new_robots[robot_id] += 1;
                 return Some(max_open_geodes(
                     cache,
                     blueprint,
-                    SearchNode {
-                        robots: new_robots,
-                        resources: new_resources,
-                        time_left: search_node.time_left - 1,
-                    },
+                    SearchNode::new(new_robots, new_resources, time_left - 1),
                 ));
             }
             return None;
@@ -100,15 +177,11 @@ fn max_open_geodes(cache: &mut Cache, blueprint: &Blueprint, search_node: Search
         .unwrap_or(0)
         .max({
             // Try not building any robot
-            let new_resources = collect_resources(&search_node.robots, &search_node.resources);
+            let new_resources = search_node.collect_resources();
             max_open_geodes(
                 cache,
                 blueprint,
-                SearchNode {
-                    robots: search_node.robots,
-                    resources: new_resources,
-                    time_left: search_node.time_left - 1,
-                },
+                SearchNode::new(robots, new_resources, time_left - 1),
             )
         });
     cache.insert(search_node, num_geodes);
@@ -125,17 +198,32 @@ fn solve_case1(blueprints: &[Blueprint]) -> u16 {
             let num_geodes = max_open_geodes(
                 &mut cache,
                 blueprint,
-                SearchNode {
-                    robots: [1, 0, 0, 0],
-                    resources: Resources::default(),
-                    time_left: 24,
-                },
+                SearchNode::new([1, 0, 0, 0], Resources::default(), 24),
             );
             println!("   Blueprint {} produces {} geodes", id, num_geodes);
             let score = (id as u16 + 1) * num_geodes;
             score
         })
         .sum()
+}
+
+fn solve_case2(blueprints: &[Blueprint]) -> u16 {
+    let mut cache = Cache::new();
+    blueprints
+        .iter()
+        .take(3)
+        .enumerate()
+        .map(|(id, blueprint)| {
+            cache.clear();
+            let num_geodes = max_open_geodes(
+                &mut cache,
+                blueprint,
+                SearchNode::new([1, 0, 0, 0], Resources::default(), 34),
+            );
+            println!("   Blueprint {} produces {} geodes", id, num_geodes);
+            num_geodes
+        })
+        .product()
 }
 
 fn main() {
@@ -145,7 +233,7 @@ fn main() {
     let input = parse("day19.input");
     aoc::expect_result!(1395, solve_case1(&input));
 
-    // println!("Part 2");
-    // aoc::expect_result!(56 * 62, solve_case2(&example));
-    // aoc::expect_result!(1337, solve_case2(&input));
+    println!("Part 2");
+    aoc::expect_result!(56 * 62, solve_case2(&example));
+    aoc::expect_result!(1337, solve_case2(&input));
 }
