b970f5873e77f3ff7451a71f4b8a703056502c4c
Peter Žužek <peterzuzek@gmail.com>
2026-06-13 09:30:18 +0200

    [C++] 2023-18 modernized

--- 2023/day18.cpp
+++ 2023/day18.cpp
@@ -1,22 +1,26 @@
 // https://adventofcode.com/2023/day/18
 
 #include "../common/common.h"
+#include "../common/rust.h"
 
-#include <algorithm>
 #include <array>
-#include <cstdint>
 #include <print>
-#include <ranges>
-#include <string>
-#include <string_view>
-#include <vector>
+#include <span>
 
-using int_t = std::int64_t;
-using point_t = aoc::point_type<int_t>;
-using trench_t = std::vector<point_t>;
+fn parse(String const& filename) -> Vec<String> {
+  return aoc::read_lines(filename);
+}
 
-int_t lagoon_size(const trench_t& trench, const int_t trench_length) {
-  auto area = aoc::calculate_area<int_t>(std::span{trench});
+using point_t = aoc::point_type<i64>;
+using trench_t = Vec<point_t>;
+
+fn trench_step(aoc::facing_t direction, i64 num_cubes)
+    -> std::pair<point_t, i64> {
+  return {aoc::get_diff<i64>(direction), num_cubes};
+}
+
+fn lagoon_size(trench_t const& trench, i64 trench_length) -> i64 {
+  let area = aoc::calculate_area<i64>(std::span{trench});
   // https://en.wikipedia.org/wiki/Pick%27s_theorem
   // A = i + b/2 - 1
   //   -> i = A - b/2 + 1
@@ -25,60 +29,14 @@ int_t lagoon_size(const trench_t& trench, const int_t trench_length) {
   return area + (trench_length / 2) + 1;
 }
 
-template <bool invert>
-int_t solve_case(const std::string& filename) {
+template <class ParseLine>
+fn solve(Vec<String> const& lines, ParseLine parse_line) -> i64 {
+  auto trench = trench_t{};
+  auto trench_length = i64{0};
+  auto current = point_t{};
 
-  const auto parse_input_p1 =
-      [&](std::string_view line) -> std::pair<point_t, int_t> {
-    const char direction = line[0];
-    auto [num_cubes_str, color_str] = aoc::split_once(line.substr(2), ' ');
-    const auto num_cubes = aoc::to_number<int_t>(num_cubes_str);
-    const auto diff = aoc::get_diff<int_t>([direction]() {
-      switch (direction) {
-        case 'R':
-          return aoc::east;
-        case 'D':
-          return aoc::south;
-        case 'L':
-          return aoc::west;
-        case 'U':
-          return aoc::north;
-        default:
-          AOC_UNREACHABLE("Invalid direction");
-          return aoc::southeast;
-      }
-    }());
-    return {diff, num_cubes};
-  };
-
-  constexpr int hex_base = 16;
-  std::array hex_direction_mapping{
-      aoc::east,
-      aoc::south,
-      aoc::west,
-      aoc::north,
-  };
-  const auto parse_input_p2 =
-      [&](std::string_view line) -> std::pair<point_t, int_t> {
-    auto [num_cubes_str, hex_str] = aoc::split_once(line.substr(2), ' ');
-    const auto diff =
-        aoc::get_diff(hex_direction_mapping[aoc::to_number(hex_str[7])]);
-    auto num_cubes = aoc::to_number<int_t>(hex_str.substr(2, 5), hex_base);
-    return {diff, num_cubes};
-  };
-
-  trench_t trench;
-  int_t trench_length = 0;
-  point_t current{};
-
-  for (std::string_view line : aoc::views::read_lines(filename)) {
-    point_t diff;
-    int num_cubes = 0;
-    if constexpr (!invert) {
-      std::tie(diff, num_cubes) = parse_input_p1(line);
-    } else {
-      std::tie(diff, num_cubes) = parse_input_p2(line);
-    }
+  for (str line : lines) {
+    let[diff, num_cubes] = parse_line(line);
     trench.push_back(current);
     current += diff * num_cubes;
     trench_length += num_cubes;
@@ -87,12 +45,58 @@ int_t solve_case(const std::string& filename) {
   return lagoon_size(trench, trench_length);
 }
 
+fn solve_case1(Vec<String> const& lines) -> i64 {
+  return solve(lines, [](str line) {
+    let direction = line[0];
+    let[num_cubes_str, color_str] = aoc::split_once(line.substr(2), ' ');
+    let num_cubes = aoc::to_number<i64>(num_cubes_str);
+    return trench_step(
+        [direction]() {
+          switch (direction) {
+            case 'R':
+              return aoc::east;
+            case 'D':
+              return aoc::south;
+            case 'L':
+              return aoc::west;
+            case 'U':
+              return aoc::north;
+            default:
+              AOC_UNREACHABLE("Invalid direction");
+              return aoc::southeast;
+          }
+        }(),
+        num_cubes);
+  });
+}
+
+fn solve_case2(Vec<String> const& lines) -> i64 {
+  static constexpr let hex_base = 16;
+  static constexpr let hex_direction_mapping = std::array{
+      aoc::east,
+      aoc::south,
+      aoc::west,
+      aoc::north,
+  };
+
+  return solve(lines, [](str line) {
+    let[num_cubes_str, hex_str] = aoc::split_once(line.substr(2), ' ');
+    let num_cubes = aoc::to_number<i64>(hex_str.substr(2, 5), hex_base);
+    return trench_step(hex_direction_mapping[aoc::to_number(hex_str[7])],
+                       num_cubes);
+  });
+}
+
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT(62, (solve_case<false>("day18.example")));
-  AOC_EXPECT_RESULT(40745, (solve_case<false>("day18.input")));
+  let example = parse("day18.example");
+  AOC_EXPECT_RESULT(62, solve_case1(example));
+  let input = parse("day18.input");
+  AOC_EXPECT_RESULT(40745, solve_case1(input));
+
   std::println("Part 2");
-  AOC_EXPECT_RESULT(952408144115, (solve_case<true>("day18.example")));
-  AOC_EXPECT_RESULT(90111113594927, (solve_case<true>("day18.input")));
+  AOC_EXPECT_RESULT(952408144115, solve_case2(example));
+  AOC_EXPECT_RESULT(90111113594927, solve_case2(input));
+
   AOC_RETURN_CHECK_RESULT();
 }
