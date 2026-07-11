f9bf3d4b418ef4ec064ff3be744502232d265944
Peter Žužek <peterzuzek@gmail.com>
2026-07-11 18:21:05 +0200

    [C++] 2024-01 modernized

--- 2024/day01.cpp
+++ 2024/day01.cpp
@@ -1,65 +1,53 @@
 // https://adventofcode.com/2024/day/1
 
 #include "../common/common.h"
+#include "../common/rust.h"
 
-#include <algorithm>
-#include <array>
 #include <print>
 #include <ranges>
-#include <string>
-#include <string_view>
-#include <vector>
 
-constexpr int sum_diffs(std::vector<int> left_list,
-                        std::vector<int> right_list) {
-  AOC_ASSERT(left_list.size() == right_list.size(), "Lists must be same size");
-  stdr::sort(left_list);
-  stdr::sort(right_list);
+struct Input {
+  Vec<i32> left;
+  Vec<i32> right;
+};
 
-  int sum = 0;
-  for (int i = 0; i < left_list.size(); ++i) {
-    sum += aoc::abs(right_list[i] - left_list[i]);
+auto parse(String const& filename) -> Input {
+  auto input = Input{};
+  for (str line : aoc::views::read_lines(filename)) {
+    let parsed = aoc::split_once<i32>(line, "   ");
+    input.left.push_back(parsed[0]);
+    input.right.push_back(parsed[1]);
   }
-  return sum;
+  return input;
+}
+
+fn sum_diffs(Vec<i32> const& left_list, Vec<i32> const& right_list) -> i32 {
+  AOC_ASSERT(left_list.size() == right_list.size(), "Lists must be same size");
+  let left = aoc::ranges::sorted(left_list);
+  let right = aoc::ranges::sorted(right_list);
+
+  return aoc::ranges::accumulate(
+      stdv::zip_transform([](i32 l, i32 r) { return aoc::abs(r - l); }, //
+                          left, right),
+      0);
 }
 
 static_assert(sum_diffs({2}, {7}) == 5);
 static_assert(sum_diffs({3, 4, 2, 1, 3, 3}, {4, 3, 5, 3, 9, 3}) == 11);
 
-constexpr int similarity_score(std::vector<int> left_list,
-                               std::vector<int> right_list) {
-  stdr::sort(left_list);
-  stdr::sort(right_list);
-
-  int previous = 0;
-  int multiplier = 0;
-  auto right_start_it = std::begin(right_list);
-  const auto right_end_it = std::end(right_list);
-
-  int sum = 0;
-  for (int i = 0; i < left_list.size(); ++i) {
-    const auto current = left_list[i];
-    if (current == previous) {
-      sum += current * multiplier;
-      continue;
-    }
-
-    auto match_it = stdr::find(right_start_it, right_end_it, current);
-    multiplier = 0;
-    while (match_it != right_end_it) {
-      if (*match_it != current) {
-        break;
-      }
-      ++multiplier;
-      ++match_it;
-      right_start_it = match_it;
-    }
-
-    sum += current * multiplier;
-    previous = current;
+fn similarity_score(Vec<i32> const& left_list, Vec<i32> const& right_list)
+    -> i32 {
+  auto right_counts = aoc::flat_map<i32, i32>{};
+  for (auto value : right_list) {
+    ++right_counts[value];
   }
 
-  return sum;
+  return aoc::ranges::accumulate(
+      left_list | stdv::transform([&](i32 value) {
+        let it = right_counts.find(value);
+        return value * (it == right_counts.end() ? 0 : it->second);
+      }),
+      0);
 }
 
 static_assert(similarity_score({3}, {4, 3, 5, 3, 9, 3}) == 9);
@@ -71,33 +59,16 @@ static_assert(similarity_score({3, 4, 2, 1}, {4, 3, 5, 3, 9, 3}) == 13);
 static_assert(similarity_score({3, 4, 2, 1, 3}, {4, 3, 5, 3, 9, 3}) == 22);
 static_assert(similarity_score({3, 4, 2, 1, 3, 3}, {4, 3, 5, 3, 9, 3}) == 31);
 
-template <bool similarity>
-int solve_case(const std::string& filename) {
-  std::vector<int> left_list;
-  std::vector<int> right_list;
-
-  for (std::string_view line : aoc::views::read_lines(filename)) {
-    auto [left, right] = aoc::split_once<int>(line, std::string_view("   "));
-    left_list.push_back(left);
-    right_list.push_back(right);
-  }
-
-  int sum = 0;
-  if constexpr (similarity) {
-    sum = similarity_score(left_list, right_list);
-  } else {
-    sum = sum_diffs(left_list, right_list);
-  }
-
-  return sum;
-}
-
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT(11, solve_case<false>("day01.example"));
-  AOC_EXPECT_RESULT(3569916, solve_case<false>("day01.input"));
+  let example = parse("day01.example");
+  AOC_EXPECT_RESULT(11, sum_diffs(example.left, example.right));
+  let input = parse("day01.input");
+  AOC_EXPECT_RESULT(3569916, sum_diffs(input.left, input.right));
+
   std::println("Part 2");
-  AOC_EXPECT_RESULT(31, solve_case<true>("day01.example"));
-  AOC_EXPECT_RESULT(26407426, solve_case<true>("day01.input"));
+  AOC_EXPECT_RESULT(31, similarity_score(example.left, example.right));
+  AOC_EXPECT_RESULT(26407426, similarity_score(input.left, input.right));
+
   AOC_RETURN_CHECK_RESULT();
 }
