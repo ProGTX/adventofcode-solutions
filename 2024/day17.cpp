959d982d1c07fb7eaab4b9d212160a7ff834ce52
Peter Žužek <peterzuzek@gmail.com>
2026-07-27 18:31:44 +0200

    [C++] 2024-17 modernized

--- 2024/day17.cpp
+++ 2024/day17.cpp
@@ -1,22 +1,19 @@
 // https://adventofcode.com/2024/day/17
 
 #include "../common/common.h"
+#include "../common/rust.h"
 
-#include <algorithm>
 #include <array>
 #include <print>
 #include <ranges>
 #include <span>
-#include <string>
-#include <string_view>
-#include <vector>
 
-constexpr const int max_3bit_end = (1 << 3);
-constexpr const int max_3bit_mask = max_3bit_end - 1;
-constexpr const std::string_view program_prefix = "Program: ";
+constexpr let max_3bit_end = (1 << 3);
+constexpr let max_3bit_mask = max_3bit_end - 1;
+constexpr let program_prefix = str{"Program: "};
 
-using registers_t = std::array<int, 3>;
-using program_t = std::vector<int>;
+using registers_t = std::array<i32, 3>;
+using program_t = Vec<i32>;
 using results_t = program_t;
 
 enum opcodes_t {
@@ -30,12 +27,36 @@ enum opcodes_t {
   cdv = 7,
 };
 
-constexpr auto run_program(std::span<const int> program,
-                           registers_t registers) {
-  int instruction_ptr = 0;
-  results_t results;
+struct Input {
+  program_t program;
+  registers_t registers;
+};
 
-  const auto combo = [&](int value) {
+auto parse(String const& filename) -> Input {
+  auto program = program_t{};
+  auto registers = registers_t{};
+
+  for (String line : aoc::views::read_lines(filename)) {
+    if (line.empty()) {
+      continue;
+    }
+    if (line.find(program_prefix) != String::npos) {
+      program = aoc::split<program_t>(line.substr(program_prefix.size()), ',');
+    } else {
+      let register_line = str{line}.substr(program_prefix.size());
+      let index = static_cast<int>(register_line[0] - 'A');
+      registers[index] = aoc::to_number<i32>(register_line.substr(3));
+    }
+  }
+
+  return {std::move(program), registers};
+}
+
+fn run_program(std::span<const i32> program, registers_t registers) {
+  int instruction_ptr = 0;
+  auto results = results_t{};
+
+  let combo = [&](i32 value) {
     AOC_ASSERT(value < 7, "Invalid combo operand");
     if (value < 4) {
       return value;
@@ -44,15 +65,15 @@ constexpr auto run_program(std::span<const int> program,
     }
   };
 
-  const auto division = [&]() {
-    const auto numerator = registers[0];
-    const auto denominator = 1 << combo(program[instruction_ptr + 1]);
+  let division = [&]() {
+    let numerator = registers[0];
+    let denominator = 1 << combo(program[instruction_ptr + 1]);
     return numerator / denominator;
   };
 
   while (instruction_ptr < program.size()) {
     AOC_ASSERT(instruction_ptr >= 0, "Invalid instruction pointer");
-    const auto op = program[instruction_ptr];
+    let op = program[instruction_ptr];
     switch (op) {
       case adv:
         registers[0] = division();
@@ -109,43 +130,27 @@ static_assert(26 == run_program(std::array{1, 7}, {0, 29, 0}).second[1]);
 static_assert(44354 ==
               run_program(std::array{4, 0}, {0, 2024, 43690}).second[1]);
 
-template <bool>
-std::string solve_case(const std::string& filename) {
-  program_t program;
-  registers_t registers;
-
-  for (std::string_view line : aoc::views::read_lines(filename)) {
-    if (line.empty()) {
-      continue;
-    }
-    if (line.find(program_prefix) != std::string::npos) {
-      program = aoc::split<program_t>(line.substr(program_prefix.size()), ',');
-    } else {
-      line = line.substr(program_prefix.size());
-      const auto index = static_cast<int>(line[0] - 'A');
-      registers[index] = aoc::to_number<int>(line.substr(3));
-    }
-  }
-
-  auto [results, new_registers] = run_program(program, registers);
-  auto result_str = results |
-                    stdv::transform([](int value) {
-                      return std::string(1, '0' + static_cast<char>(value));
-                    }) |
-                    stdv::join_with(',') |
-                    aoc::collect_string();
-  return result_str;
+fn solve_case1(Input const& input) -> String {
+  let[results, new_registers] = run_program(input.program, input.registers);
+  return results |
+         stdv::transform([](i32 value) {
+           return String(1, '0' + static_cast<char>(value));
+         }) |
+         stdv::join_with(',') |
+         aoc::collect_string();
 }
 
 int main() {
   std::println("Part 1");
-  AOC_EXPECT_RESULT("4,6,3,5,6,3,5,2,1,0", solve_case<false>("day17.example"));
-  AOC_EXPECT_RESULT("1,7,6,5,1,0,5,0,7", solve_case<false>("day17.input"));
+  let example = parse("day17.example");
+  AOC_EXPECT_RESULT("4,6,3,5,6,3,5,2,1,0", solve_case1(example));
+  let input = parse("day17.input");
+  AOC_EXPECT_RESULT("1,7,6,5,1,0,5,0,7", solve_case1(input));
 
   std::println("Part 2");
   aoc::return_incomplete();
-  // AOC_EXPECT_RESULT(281, solve_case<true>("day17.example"));
-  // AOC_EXPECT_RESULT(53515, solve_case<true>("day17.input"));
+  // AOC_EXPECT_RESULT(117440, solve_case2(example));
+  // AOC_EXPECT_RESULT(1337, solve_case2(input));
 
   AOC_RETURN_CHECK_RESULT();
 }
