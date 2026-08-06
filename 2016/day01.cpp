ee14e462f894f47df858009331e90463a45d9d08
Peter Žužek <peterzuzek@gmail.com>
2026-08-06 21:33:08 +0200

    2016 scaffolding

---
+++ 2016/day01.cpp
@@ -0,0 +1,15 @@
+// https://adventofcode.com/2016/day/1
+
+#include "../common/common.h"
+#include "../common/rust.h"
+
+#ifndef AOC_IMPORT_STD
+#include <print>
+#endif
+
+int main() {
+  std::println("Part 1");
+  aoc::return_incomplete();
+
+  AOC_RETURN_CHECK_RESULT();
+}
