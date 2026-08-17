39e9e6df4a19231b48cda684f8e72571b5dd044c
Peter Žužek <peterzuzek@gmail.com>
2026-08-17 21:19:18 +0200

    [Rust] 2016-03 part 2

--- rust/2016/src/day03.rs
+++ rust/2016/src/day03.rs
@@ -16,23 +16,41 @@ fn parse(filename: &str) -> Input {
         .collect()
 }
 
+fn is_triangle([a, b, c]: &[u32; 3]) -> bool {
+    let is_sum_ok = |a, b, target: &u32| (a + b) > *target;
+    is_sum_ok(a, b, c) && is_sum_ok(a, c, b) && is_sum_ok(b, c, a)
+}
+
 fn solve_case1(triangles: &Input) -> usize {
-    let is_sum_ok = |a, b, target| (a + b) > target;
+    triangles.iter().filter(|&t| is_triangle(t)).count()
+}
+
+fn solve_case2(triangles: &Input) -> usize {
+    debug_assert!(
+        triangles.len() % 3 == 0,
+        "Reading 3 triangle lines at a time"
+    );
     triangles
-        .iter()
-        .filter(|&&[a, b, c]| is_sum_ok(a, b, c) && is_sum_ok(a, c, b) && is_sum_ok(b, c, a))
-        .count()
+        .chunks(3)
+        .map(|chunk| {
+            let t0 = chunk[0];
+            let t1 = chunk[1];
+            let t2 = chunk[2];
+            return (is_triangle(&[t0[0], t1[0], t2[0]])) as usize
+                + (is_triangle(&[t0[1], t1[1], t2[1]])) as usize
+                + (is_triangle(&[t0[2], t1[2], t2[2]])) as usize;
+        })
+        .sum()
 }
 
 fn main() {
     println!("Part 1");
     let example = parse("day03.example");
-    aoc::expect_result!(1, solve_case1(&example));
+    aoc::expect_result!(3, solve_case1(&example));
     let input = parse("day03.input");
     aoc::expect_result!(993, solve_case1(&input));
 
     println!("Part 2");
-    aoc::return_incomplete();
-    // aoc::expect_result!(1337, solve_case2(&example));
-    // aoc::expect_result!(1337, solve_case2(&input));
+    aoc::expect_result!(6, solve_case2(&example));
+    aoc::expect_result!(1849, solve_case2(&input));
 }
