3a1abdcde243b2f1f925a152fda9bf2d1aabb566
Peter Žužek <peterzuzek@gmail.com>
2026-08-18 02:39:10 +0200

    [Rust] 2016-04 part 2

--- rust/2016/src/day04.rs
+++ rust/2016/src/day04.rs
@@ -1,4 +1,5 @@
 use aoc::iter::CollectArrayVec;
+use itertools::Itertools;
 
 struct Room {
     name: String,
@@ -6,19 +7,19 @@ struct Room {
     checksum: [char; 5],
 }
 
+// This char chosen because it appears before 'a'
+const SEPARATOR: char = '`';
+
 type Input = Vec<Room>;
 
 fn parse(filename: &str) -> Input {
+    let sep_str = format!("{}", SEPARATOR);
     aoc::file::read_lines(filename)
         .iter()
         .map(|line| {
             let parts = line.split('-').collect::<Vec<_>>();
             let num_parts = parts.len();
-            let name = parts
-                .iter()
-                .take(num_parts - 1)
-                .flat_map(|&part| part.chars())
-                .collect();
+            let name = parts.iter().take(num_parts - 1).join(&sep_str);
             let tail = parts
                 .iter()
                 .skip(num_parts - 1)
@@ -39,16 +40,23 @@ fn parse(filename: &str) -> Input {
         .collect()
 }
 
+const NUM_LETTERS: usize = 26;
+
 fn solve_case1(rooms: &Input) -> usize {
-    type FreqMap = [usize; 26];
     rooms
         .iter()
         .filter_map(|room| {
-            let mut frequency_map = FreqMap::default();
+            let mut frequency_map = <[usize; NUM_LETTERS]>::default();
             for letter in room.name.bytes() {
+                if letter == (SEPARATOR as u8) {
+                    continue;
+                }
                 frequency_map[(letter - b'a') as usize] += 1;
             }
-            let mut sorted = frequency_map.iter().enumerate().collect_array_vec::<26>();
+            let mut sorted = frequency_map
+                .iter()
+                .enumerate()
+                .collect_array_vec::<NUM_LETTERS>();
             sorted.sort_by(|(lhs_id, lhs), (rhs_id, rhs)| rhs.cmp(lhs).then(lhs_id.cmp(rhs_id)));
             sorted
                 .into_iter()
@@ -59,6 +67,26 @@ fn solve_case1(rooms: &Input) -> usize {
         .sum()
 }
 
+fn solve_case2(rooms: &Input) -> usize {
+    for room in rooms {
+        let real_name = room
+            .name
+            .bytes()
+            .map(|letter| {
+                if letter == (SEPARATOR as u8) {
+                    ' '
+                } else {
+                    ((((letter - b'a') as usize + room.sector) % NUM_LETTERS) as u8 + b'a') as char
+                }
+            })
+            .collect::<String>();
+        if real_name == "northpole object storage" {
+            return room.sector;
+        }
+    }
+    0
+}
+
 fn main() {
     println!("Part 1");
     let example = parse("day04.example");
@@ -67,7 +95,6 @@ fn main() {
     aoc::expect_result!(173787, solve_case1(&input));
 
     println!("Part 2");
-    aoc::return_incomplete();
-    // aoc::expect_result!(1337, solve_case2(&example));
-    // aoc::expect_result!(1337, solve_case2(&input));
+    aoc::expect_result!(0, solve_case2(&example));
+    aoc::expect_result!(548, solve_case2(&input));
 }
