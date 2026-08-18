d8296aaaf171d88a70dc7c88e127a5d247418421
Peter Žužek <peterzuzek@gmail.com>
2026-08-19 00:27:42 +0200

    [Rust] 2016-06 part 2

--- rust/2016/src/day06.rs
+++ rust/2016/src/day06.rs
@@ -8,14 +8,23 @@ fn parse(filename: &str) -> Input {
     Input::from_file(filename)
 }
 
-fn solve_case1(msg_grid: &Input) -> String {
+const LEAST: bool = false;
+const MOST: bool = true;
+
+fn solve_case<const ORDER: bool>(msg_grid: &Input) -> String {
     (0..msg_grid.num_columns)
         .map(|col| {
             msg_grid
                 .column(col)
                 .counts_with_hasher(FxBuildHasher)
                 .into_iter()
-                .max_by_key(|(_, count)| *count)
+                .max_by(|(_, lhs), (_, rhs)| {
+                    if ORDER == MOST {
+                        lhs.cmp(rhs)
+                    } else {
+                        rhs.cmp(lhs)
+                    }
+                })
                 .unwrap()
                 .0
         })
@@ -25,11 +34,11 @@ fn solve_case1(msg_grid: &Input) -> String {
 fn main() {
     println!("Part 1");
     let example = parse("day06.example");
-    aoc::expect_result!("easter", solve_case1(&example));
+    aoc::expect_result!("easter", solve_case::<MOST>(&example));
     let input = parse("day06.input");
-    aoc::expect_result!("umejzgdw", solve_case1(&input));
+    aoc::expect_result!("umejzgdw", solve_case::<MOST>(&input));
 
     println!("Part 2");
-    //aoc::expect_result!(1337, solve_case2(&example));
-    //aoc::expect_result!(1337, solve_case2(&input));
+    aoc::expect_result!("advent", solve_case::<LEAST>(&example));
+    aoc::expect_result!("aovueakv", solve_case::<LEAST>(&input));
 }
