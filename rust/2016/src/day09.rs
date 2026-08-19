e4f008af57add6270dbe43a53483afb6e97f155a
Peter Žužek <peterzuzek@gmail.com>
2026-08-19 23:02:02 +0200

    [Rust] 2016-09 simplified

--- rust/2016/src/day09.rs
+++ rust/2016/src/day09.rs
@@ -5,27 +5,24 @@ fn parse(filename: &str) -> String {
 fn decompressed_len<const RECURSION: bool>(compressed: &str) -> usize {
     let mut count = 0;
     let mut slice = compressed;
-    while !slice.is_empty() {
-        if let Some(open) = slice.find('(') {
-            count += open;
-            let close = slice.find(')').unwrap();
-            debug_assert!(close > open, "Invalid parenthesis");
-            let repetition = &slice[open + 1..close].split_once('x').unwrap();
-            let num_chars = repetition.0.parse::<usize>().unwrap();
-            let num_repeats = repetition.1.parse::<usize>().unwrap();
-            let repeat_end_index = close + 1 + num_chars;
-            let subslice_len = if !RECURSION {
-                num_chars
-            } else {
-                decompressed_len::<RECURSION>(&slice[close + 1..repeat_end_index])
-            };
-            count += subslice_len * num_repeats;
-            slice = &slice[repeat_end_index..];
+    while let Some(open) = slice.find('(') {
+        count += open;
+        let close = slice.find(')').unwrap();
+        debug_assert!(close > open, "Invalid parenthesis");
+        let repetition = &slice[open + 1..close].split_once('x').unwrap();
+        let num_chars = repetition.0.parse::<usize>().unwrap();
+        let num_repeats = repetition.1.parse::<usize>().unwrap();
+        let repeat_end_index = close + 1 + num_chars;
+        let subslice_len = if !RECURSION {
+            num_chars
         } else {
-            count += slice.len();
-            break;
-        }
+            decompressed_len::<RECURSION>(&slice[close + 1..repeat_end_index])
+        };
+        count += subslice_len * num_repeats;
+        slice = &slice[repeat_end_index..];
     }
+    count += slice.len();
+
     return count;
 }
 
