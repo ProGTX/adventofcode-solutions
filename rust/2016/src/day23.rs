beaaa52f172f770dee8deac5b3e0a84af6ed6bfc
Peter Žužek <peterzuzek@gmail.com>
2026-08-29 22:29:34 +0200

    [Rust] 2016-23 final optimization
    Transform the outer loop,
    brings the time down to zero.

--- rust/2016/src/day23.rs
+++ rust/2016/src/day23.rs
@@ -1,32 +1,21 @@
 mod assembunny;
-use assembunny::{Scalar, Value};
+use assembunny::{Registers, Scalar, Value};
 use itertools::Itertools;
 
 /// Transformed instructions
 #[derive(Clone, Copy, Debug)]
 pub enum XOp {
+    NoOp,
     Zero(usize),
     ZeroToggled(usize),
     Add(usize, Scalar),
-    Fma(usize, usize, Value), // out_id, in_id, input_val
+    LoopAdd(usize, usize, Value), // out_id, loop_reg, steps
     Copy(Value, Value),
     JumpNotZero(Value, Value),
     Toggle(usize),
 }
 
 type Input = Vec<XOp>;
-
-/// Prints the original and the transformed instructions side by side
-fn print(all_ops: &[Input; 2]) {
-    const WIDTH: usize = 40;
-    for (index, (original, transformed)) in all_ops[0].iter().zip(&all_ops[1]).enumerate() {
-        let original = format!("{original:?}");
-        let transformed = format!("{transformed:?}");
-        // Mark the instructions that the loop transformation changed
-        let marker = if (original == transformed) { ' ' } else { '*' };
-        println!("{index:3}: {original:<WIDTH$} {marker} {transformed}");
-    }
-}
 
 fn parse(filename: &str) -> Input {
     type Op = assembunny::Op;
@@ -43,7 +32,6 @@ fn parse(filename: &str) -> Input {
             Op::Toggle(x) => XOp::Toggle(x),
         })
         .collect_vec();
-    let mut all_ops = [ops.clone(), Vec::with_capacity(ops.len())];
 
     // Transform loops
     let mut search_from = 0;
@@ -62,18 +50,25 @@ fn parse(filename: &str) -> Input {
 
         // offset is negative here
         let start = index - loop_offset.unsigned_abs() as usize;
-        // If we're decrementing register id within the last offset instructions,
-        // we know this is a loop
-        // NOTE: This assumes that the value of the register is positive
-        let is_loop = (ops[start..index]
+
+        if !ops[start..index]
             .iter()
-            .any(|&op| matches!(op, XOp::Add(id, -1) if (id == loop_reg))))
-            && (
-                // Only adds are supported in a loop
-                ops[start..index]
-                    .iter()
-                    .all(|&op| matches!(op, XOp::Add(_, _)))
-            );
+            .all(|&op| matches!(op, XOp::Add(_, _)))
+        {
+            // Only adds are supported in a loop
+            continue;
+        }
+
+        // If we're incrementing or decrementing register id by 1
+        // within the last offset instructions, we know this is a loop
+        // NOTE: This assumes that the value of the register
+        // is opposite of the sign of the increment/decrement
+        let is_loop_counter = |id, value: Scalar| {
+            return (id == loop_reg) && (value.abs() == 1);
+        };
+        let is_loop = ops[start..index]
+            .iter()
+            .any(|&op| matches!(op, XOp::Add(id, value) if is_loop_counter(id, value) ));
         if !is_loop {
             continue;
         }
@@ -82,38 +77,86 @@ fn parse(filename: &str) -> Input {
         let mut new_ops = Vec::with_capacity(index + 1 - start);
         for op in &ops[start..index] {
             match *op {
-                XOp::Add(id, -1) if (id == loop_reg) => {
-                    // This is our loop counter, zero out at the end
+                XOp::Add(id, value) if is_loop_counter(id, value) => {
+                    // Zero out loop counter as part of LoopAdd
                 }
                 XOp::Add(id, value) => {
-                    new_ops.push(XOp::Fma(id, loop_reg, Value::Literal(value)));
+                    new_ops.push(XOp::LoopAdd(id, loop_reg, Value::Literal(value)));
                 }
                 _ => unreachable!(),
             }
         }
-        new_ops.push(XOp::Zero(loop_reg));
-        // Add a no-op to keep the number of instructions the same
-        new_ops.push(XOp::Add(loop_reg, 0));
+        // Keep the number of instructions the same,
+        // the offsets held in registers depend on it
+        new_ops.push(XOp::NoOp); // Replaces loop counter
+        new_ops.push(XOp::NoOp); // Replaces jump op
 
         ops[start..=index].copy_from_slice(&new_ops);
     }
 
-    all_ops[1] = ops.clone();
-    print(&all_ops);
+    // Transform a very specific loop
+    const SPECIFIC_LOOP_OFFSET: i32 = -5;
+    if let Some((index, outer_reg)) = //
+        ops.iter().enumerate().find_map(|(index, op)| match *op {
+            XOp::JumpNotZero(Value::Register(id), Value::Literal(value))
+                if (value == SPECIFIC_LOOP_OFFSET) =>
+            {
+                Some((index, id))
+            }
+            _ => None,
+        })
+    {
+        let start = index - SPECIFIC_LOOP_OFFSET.unsigned_abs() as usize;
+        assert!(
+            matches!(
+                ops[start],
+                XOp::Copy(Value::Register(_), Value::Register(_))
+            ),
+            "Unexpected input: {:?}",
+            ops[start]
+        );
+        let (out_id, inner_reg) = match ops[start + 1] {
+            XOp::LoopAdd(out_id, loop_reg, Value::Literal(1)) => (out_id, loop_reg),
+            _ => unreachable!("Unexpected input: {:?}", ops[start + 1]),
+        };
+        assert!(
+            matches!(ops[start + 2], XOp::NoOp),
+            "Expected NoOp, got {:?}",
+            ops[start + 2]
+        );
+        assert!(
+            matches!(ops[start + 3], XOp::NoOp),
+            "Expected NoOp, got {:?}",
+            ops[start + 3]
+        );
+        match ops[start + 4] {
+            XOp::Add(loop_reg, value) => {
+                assert_eq!(outer_reg, loop_reg, "Loop register doesn't match");
+                assert_eq!(1, value.abs(), "Loop increment not 1");
+            }
+            _ => unreachable!("Unexpected input: {:?}", ops[start + 4]),
+        }
+
+        // ops[start]: Keep the copy as-is
+        ops[start + 1] = XOp::LoopAdd(out_id, inner_reg, Value::Register(outer_reg));
+        // Keep the number of instructions the same,
+        // the offsets held in registers depend on it
+        ops[start + 2] = XOp::NoOp;
+        ops[start + 3] = XOp::NoOp;
+        ops[start + 4] = XOp::NoOp;
+        ops[index] = XOp::NoOp;
+    }
 
     return ops;
 }
 
 fn toggle(op: XOp) -> XOp {
     match op {
+        XOp::NoOp => XOp::NoOp,
         XOp::Zero(id) => XOp::ZeroToggled(id),
         XOp::ZeroToggled(id) => XOp::Zero(id),
         // Add replaces Increase and Decrease
         XOp::Add(id, value) => XOp::Add(id, -value),
-        XOp::Fma(_, _, _) => {
-            unimplemented!("Not sure how to toggle this op: {:?}", op)
-        }
-        // Regular instructions
         XOp::Toggle(x) => XOp::Add(x, 1),
         XOp::JumpNotZero(condition, offset) => XOp::Copy(condition, offset),
         XOp::Copy(from, to) => XOp::JumpNotZero(from, to),
@@ -124,7 +167,7 @@ fn toggle(op: XOp) -> XOp {
 /// The value in register a once the program halts
 fn solve_case<const NUM_EGGS: i64>(ops: &Input) -> i64 {
     let mut ops = ops.clone();
-    let mut registers = [NUM_EGGS, 0, 0, 0];
+    let mut registers: Registers = [NUM_EGGS, 0, 0, 0];
     let mut counter = 0_i64;
     while ((counter >= 0) && ((counter as usize) < ops.len())) {
         let op = ops[counter as usize];
@@ -143,9 +186,11 @@ fn solve_case<const NUM_EGGS: i64>(ops: &Input) -> i64 {
         match op {
             XOp::Zero(id) => registers[id] = 0,
             XOp::Add(id, value) => registers[id] += value as i64,
-            XOp::Fma(out_id, in_id, input_val) => {
+            XOp::LoopAdd(out_id, loop_reg, steps) => {
                 // Fused multiply-add
-                registers[out_id] += registers[in_id] * assembunny::read(&registers, input_val);
+                registers[out_id] += registers[loop_reg] * assembunny::read(&registers, steps);
+                // Loop counter is zero at the end
+                registers[loop_reg] = 0;
             }
             XOp::Copy(from, Value::Register(to)) => {
                 registers[to] = assembunny::read(&registers, from)
